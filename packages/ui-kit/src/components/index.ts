ed211ad3f965b0f817880d7e7778e7e119956017
Mohammad Kermani <mkermani144@gmail.com>
2023-06-17 11:25:17 +0330

    refactor(ui-kit): use `img` element instead of mui `Avatar` for `AppLogo`
    Mui avatar has some styling issues (such as default circular/square
    shape) which makes it inappropriate for Rosen logos which have a title
    and are not square shaped.
    
    Use HTML `img` elements for rendering the logo and make it infer its
    height based on the provided width and the image ratio.
    
    In addition:
    - remove `title` prop as it is not needed
    - rename component to `AppLogo`

--- packages/ui-kit/src/components/index.ts
+++ packages/ui-kit/src/components/index.ts
@@ -1,6 +1,6 @@
 export * from './base';
 
-export * from './common/AppAvatar';
+export * from './common/AppLogo';
 export * from './common/WidgetCardBase';
 export * from './common/AppBar';
 export * from './common/NavigationButton';
