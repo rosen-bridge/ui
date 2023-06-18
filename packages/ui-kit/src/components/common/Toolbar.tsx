23dbbd7dccba8b51ede5344d50d78aca2aedec08
Mehdi jamshidian <jamshidian@outlook.com>
2023-06-17 19:44:53 -0700

    feat: add title and decription centering props to the toolabr

--- packages/ui-kit/src/components/common/Toolbar.tsx
+++ packages/ui-kit/src/components/common/Toolbar.tsx
@@ -39,6 +39,7 @@ const Header = styled(Box)(({ theme }) => ({
 export interface ToolbarProps {
   title: string;
   description: string;
+  isCentered?: boolean;
   toolbarActions?: ReactNode;
 }
 
@@ -46,13 +47,13 @@ export interface ToolbarProps {
  *
  * @param title - layout title to be shown in the header
  * @param description - layout description to be shown in the header
- *
+ * @param isCentered- if true, the title and description will be rendered in the center of the toolbar
  * @param toolbarActions- react node to show at the end of the toolbar in the desktop.
  * the toolbar actions are not visible in mobile and tablet
  */
 
 export const Toolbar: React.FC<ToolbarProps> = (props) => {
-  const { title, description, toolbarActions } = props;
+  const { title, description, toolbarActions, isCentered } = props;
 
   const renderActions = () => (
     <ToolbarActions> {toolbarActions} </ToolbarActions>
@@ -60,7 +61,7 @@ export const Toolbar: React.FC<ToolbarProps> = (props) => {
 
   return (
     <Header>
-      <Box flexGrow={1}>
+      <Box flexGrow={1} {...(isCentered ? { justifyContent: 'center' } : {})}>
         <Typography variant="h1">{title}</Typography>
         <Typography color="textSecondary">{description}</Typography>
       </Box>
