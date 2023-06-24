5af442ead3b38980ac5d4fb7d073f4087d0a711d
Mohammad Kermani <mkermani144@gmail.com>
2023-06-24 05:10:40 +0000

    feat(watcher-app): add actions section in home page

--- apps/watcher/app/(home)/layout.tsx
+++ apps/watcher/app/(home)/layout.tsx
@@ -4,18 +4,13 @@ import { Grid } from '@rosen-bridge/ui-kit';
 
 import { LayoutProps } from '@/_types';
 
-const HomeLayout = ({ infoWidgets }: LayoutProps) => (
+const HomeLayout = ({ actions, infoWidgets }: LayoutProps) => (
   <Grid container spacing={{ mobile: 1, tablet: 3 }}>
     <Grid item mobile={12}>
       {infoWidgets}
     </Grid>
-    <Grid item container mobile={12} tablet={6}>
-      <Grid item>
-        {/**
-         * TODO: actions element comes here
-         * https://git.ergopool.io/ergo/rosen-bridge/ui/-/issues/9
-         */}
-      </Grid>
+    <Grid item container direction="column" mobile={12} tablet={6}>
+      <Grid item>{actions}</Grid>
       <Grid item>
         {/**
          * TODO: revenues element comes here
