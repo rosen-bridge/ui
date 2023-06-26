2664be265c517d5b0a80531584e96723e8999dfa
Mohammad Kermani <mkermani144@gmail.com>
2023-06-26 10:23:57 +0000

    feat(watcher-app): add address section to home page

--- apps/watcher/app/(home)/@address/page.tsx
+++ apps/watcher/app/(home)/@address/page.tsx
@@ -1,9 +1,55 @@
 'use client';
 
-/**
- * TODO: Implement component as part of #9
- * https://git.ergopool.io/ergo/rosen-bridge/ui/-/issues/9
- */
-const Address = () => null;
+import React from 'react';
+import useSWR from 'swr';
+
+import { Box, Card, FullCard, Grid, Typography } from '@rosen-bridge/ui-kit';
+
+import AddressSkeleton from './AddressSkeleton';
+import CopyButton from './CopyButton';
+import QrCodeButton from './QrCodeButton';
+
+import fetcher from '@/_utils/fetcher';
+
+import { ApiInfoResponse } from '@/_types/api';
+
+const Address = () => {
+  const { data, isLoading } = useSWR<ApiInfoResponse>('/info', fetcher);
+
+  return (
+    <FullCard title="Address">
+      {isLoading ? (
+        <AddressSkeleton />
+      ) : (
+        data && (
+          <Card variant="outlined" sx={{ p: 1, mx: 1 }}>
+            <Grid container justifyContent="space-between">
+              <Grid item>
+                <Typography>{data.address}</Typography>
+              </Grid>
+              <Grid item>
+                <Box
+                  sx={{
+                    m: -1,
+                    ml: 'auto',
+                    flexShrink: 0,
+                    bgcolor: (theme) =>
+                      theme.palette.mode === 'light'
+                        ? '#00000011'
+                        : '#ffffff11',
+                    borderLeft: (theme) => `1px solid ${theme.palette.divider}`,
+                  }}
+                >
+                  <CopyButton address={data.address} />
+                  <QrCodeButton address={data.address} />
+                </Box>
+              </Grid>
+            </Grid>
+          </Card>
+        )
+      )}
+    </FullCard>
+  );
+};
 
 export default Address;
