ac7f97b1ba29ce38c1fe4a3b83acfe5a571f5354
Mohammad Kermani <mkermani144@gmail.com>
2023-07-05 12:05:15 +0000

    refactor: extract tokens list component logic from watcher app to ui kit

--- apps/watcher/app/(home)/@tokens/page.tsx
+++ apps/watcher/app/(home)/@tokens/page.tsx
@@ -2,11 +2,8 @@
 
 import useSWR from 'swr';
 
-import { Button, FullCard, List } from '@rosen-bridge/ui-kit';
+import { TokensCard } from '@rosen-bridge/ui-kit';
 import { fetcher } from '@rosen-ui/swr-helpers';
-
-import TokenListItem from './TokenListItem';
-import TokenListItemSkeleton from './TokenListItemSkeleton';
 
 import { ApiAddressAssetsResponse } from '@/_types/api';
 
@@ -17,42 +14,7 @@ const Tokens = () => {
   );
 
   return (
-    <FullCard
-      title="Tokens"
-      headerActions={
-        <Button
-          /**
-           * TODO: Implement click handler as part of #9
-           * https://git.ergopool.io/ergo/rosen-bridge/ui/-/issues/9
-           */
-          size="small"
-        >
-          See All
-        </Button>
-      }
-    >
-      {tokens ? (
-        <List>
-          {tokens.map((token, index) => (
-            <TokenListItem
-              decimals={token.decimals}
-              id={token.tokenId}
-              index={index}
-              key={token.tokenId}
-              name={token.name}
-              value={token.amount.toString()}
-            />
-          ))}
-        </List>
-      ) : (
-        isLoading && (
-          <List>
-            <TokenListItemSkeleton />
-            <TokenListItemSkeleton />
-          </List>
-        )
-      )}
-    </FullCard>
+    <TokensCard tokens={tokens ?? []} isLoading={isLoading} title="Tokens" />
   );
 };
 
