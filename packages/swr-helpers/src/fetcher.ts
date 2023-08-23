751bc8d93378e1e4efbdbd7762db5f283bc4e668
Mohammad Kermani <mkermani144@gmail.com>
2023-08-23 11:49:33 +0330

    chore(swr-helpers): switch to a hard-coded baseURL
    Based on the current state of our apps, they need to work in a docker
    container. To do so, set `baseURL` to a hard-coded `'/api'` route and
    let the docker container handle the redirection from the mentioned route
    to accessible service routes.
    
    Note that this is a temporary solution and need to be changed in favor
    of a dynamic baseURL which can be set in the apps based on user config.

--- packages/swr-helpers/src/fetcher.ts
+++ packages/swr-helpers/src/fetcher.ts
@@ -2,6 +2,13 @@ import axios from 'axios';
 import JSONBigInt from 'json-bigint';
 
 /**
+ * FIXME: Remove this in favor of a dynamic baseURL
+ *
+ * https://git.ergopool.io/ergo/rosen-bridge/ui/-/issues/43
+ */
+axios.defaults.baseURL = '/api';
+
+/**
  * wrap `axios.get`, returning data field of response
  * @param url
  * @param params
