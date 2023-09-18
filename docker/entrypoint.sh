b6f3139398240096263859773acac6bb4013d7e8
Arsalan Yavari <arya48.yavari79@gmail.com>
2023-09-18 11:15:02 +0330

    convert hostname in entrypoint script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,5 +2,6 @@
 
 sed -i 's/SERVER_NAME_PLACEHOLDER/'"$SERVICE_NAME"'/g' /etc/nginx/nginx.conf
 sed -i 's/SERVER_PORT_PLACEHOLDER/'"$SERVICE_PORT"'/g' /etc/nginx/nginx.conf
+sed -i 's/HOSTNAME/'"$HOSTNAME"'/g' /etc/nginx/nginx.conf
 
 exec nginx -g "daemon off;"
