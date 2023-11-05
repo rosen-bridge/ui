4933ff6a536ad67466867e3e72c94031ca8c7eea
Mohammad Kermani <mkermani144@gmail.com>
2023-11-05 06:56:26 +0000

    chore: remove redundant components
    Also update wrong TODO comment in some of empty components.

--- apps/guard/app/(dashboard)/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-/**
- * TODO: Implement component as part of #10
- * local:ergo/rosen-bridge/ui#10
- */
-const HomePage = () => null;
-
-export default HomePage;
--- apps/watcher/app/actions/@text/stop/page.tsx
+++ apps/watcher/app/actions/@text/stop/page.tsx
@@ -1,9 +1,5 @@
 'use client';
 
-/**
- * TODO: Implement component as part of #9
- * local:ergo/rosen-bridge/ui#9
- */
 const StopText = () => null;
 
 export default StopText;
