501896b79f081f3bcdc84d601a10e0886d51b6b9
HFazelinia <hadifazelinia78@gmail.com>
2024-04-05 23:12:07 +0100

    integrate bitcoin into rosen-app

---
+++ apps/rosen/app/_networks/bitcoin/transaction/constants.ts
@@ -0,0 +1,13 @@
+export const SEGWIT_INPUT_WEIGHT_UNIT = 272;
+export const SEGWIT_OUTPUT_WEIGHT_UNIT = 124;
+export const CONFIRMATION_TARGET = 6;
+export const BITCOIN_CHAIN = 'bitcoin';
+export const ETHEREUM_CHAIN = 'ethereum';
+export const CARDANO_CHAIN = 'cardano';
+export const ERGO_CHAIN = 'ergo';
+export const SUPPORTED_CHAINS = [
+  ERGO_CHAIN,
+  CARDANO_CHAIN,
+  BITCOIN_CHAIN,
+  ETHEREUM_CHAIN,
+];
