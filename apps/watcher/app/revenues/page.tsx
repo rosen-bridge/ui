6ce637c0fa8107c85660983107678b1b527d5ec1
Mohammad Kermani <mkermani144@gmail.com>
2024-04-16 07:51:20 +0000

    refactor(ui-kit): add default rowsPerPageOptions to EnhancedTable
    Remove the prop from all places that EnhancedTable is used with the
    default prop.

--- apps/watcher/app/revenues/page.tsx
+++ apps/watcher/app/revenues/page.tsx
@@ -72,7 +72,6 @@ const Revenues = () => {
 
   const paginationProps = useMemo<TablePaginationProps>(
     () => ({
-      rowsPerPageOptions: [5, 10, 25, 100],
       component: 'div',
       count: data?.total ?? 0,
       rowsPerPage: pageSize,
