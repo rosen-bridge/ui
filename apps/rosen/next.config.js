4e74d1fa289607749c17f5624889feb0a5fde94f
abdolian <m.abdolian@gmail.com>
2024-04-29 13:58:35 +0330

    fix: Update the headers section in the next.config.js file.

--- apps/rosen/next.config.js
+++ apps/rosen/next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/',
+        source: '/(.*?)',
         headers: [
           {
             key: 'X-Frame-Options',
