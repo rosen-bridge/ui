8e27994195c783d37047b6ae9118fd27f8e134ed
Masood Abdolian <m.abdolian@gmail.com>
2024-06-09 11:14:23 +0330

    chore: Replace nami logic in the cardano network

---
+++ .changeset/thick-eyes-hear.md
@@ -0,0 +1,6 @@
+---
+'@rosen-ui/nami-wallet': patch
+'@rosen-bridge/rosen-app': patch
+---
+
+Refactor the Nami wallet logic to reside within its own package
--- apps/rosen/app/_networks/cardano/server.ts
+++ apps/rosen/app/_networks/cardano/server.ts
@@ -1,15 +1,26 @@
 'use server';
 
 import { generateUnsignedTx as generateUnsignedTxEternlCore } from '@rosen-ui/eternl-wallet/dist/src/generateUnsignedTx';
-
 import {
   decodeWasmValue as decodeWasmValueCore,
   generateLockAuxiliaryData as generateLockAuxiliaryDataEternlCore,
   setTxWitnessSet as setTxWitnessSetEternlCore,
 } from '@rosen-ui/eternl-wallet/dist/src/utils';
 
+import { generateUnsignedTx as generateUnsignedTxNamiCore } from '@rosen-ui/nami-wallet/dist/src/generateUnsignedTx';
+import {
+  decodeWasmValue as decodeWasmValueNamiCore,
+  generateLockAuxiliaryData as generateLockAuxiliaryDataNamiCore,
+  setTxWitnessSet as setTxWitnessSetNamiCore,
+} from '@rosen-ui/nami-wallet/dist/src/utils';
+
 export const decodeWasmValue = decodeWasmValueCore;
 export const generateLockAuxiliaryDataEternl =
   generateLockAuxiliaryDataEternlCore;
 export const generateUnsignedTxEternl = generateUnsignedTxEternlCore;
 export const setTxWitnessSetEternl = setTxWitnessSetEternlCore;
+
+export const decodeWasmValueNami = decodeWasmValueNamiCore;
+export const generateLockAuxiliaryDataNami = generateLockAuxiliaryDataNamiCore;
+export const generateUnsignedTxNami = generateUnsignedTxNamiCore;
+export const setTxWitnessSetNami = setTxWitnessSetNamiCore;
