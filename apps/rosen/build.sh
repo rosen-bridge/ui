4771c34245374370df602fbd13b81042a8c45c9a
Masood Abdolian <m.abdolian@gmail.com>
2024-06-11 14:22:34 +0330

    fix: Update build command

--- apps/rosen/build.sh
+++ apps/rosen/build.sh
@@ -13,8 +13,8 @@ npm run build --workspace packages/constants
 npm run build --workspace packages/types
 npm run build --workspace packages/shared-contexts
 npm run build --workspace packages --if-present
-npm run build --workspace networks/cardano
 npm run build --workspace wallets/wallet-api
+npm run build --workspace networks/cardano
 npm run build --workspace wallets/nami-wallet
 npm run build --workspace wallets/lace-wallet
 npm run build --workspace wallets/eternl-wallet
