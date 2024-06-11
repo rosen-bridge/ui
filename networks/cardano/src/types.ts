c42f3357765bfa443826eafb6df6e01d52f649cb
Masood Abdolian <m.abdolian@gmail.com>
2024-06-11 13:20:07 +0330

    feat: Implement the Cardano network package

---
+++ networks/cardano/src/types.ts
@@ -0,0 +1,29 @@
+import { Wallet } from '@rosen-ui/wallet-api';
+
+import { generateUnsignedTx } from './generateUnsignedTx';
+import {
+  decodeWasmValue,
+  generateLockAuxiliaryData,
+  setTxWitnessSet,
+} from './utils';
+
+export interface CardanoProtocolParams {
+  min_fee_a: number;
+  min_fee_b: number;
+  pool_deposit: string;
+  key_deposit: string;
+  max_value_size: number;
+  max_tx_size: number;
+  coins_per_utxo_size: string;
+}
+
+export const ADA_POLICY_ID = '';
+
+export type WalletCreator = (config: WalletCreatorConfig) => Wallet | undefined;
+
+export type WalletCreatorConfig = {
+  decodeWasmValue: typeof decodeWasmValue;
+  generateLockAuxiliaryData: typeof generateLockAuxiliaryData;
+  generateUnsignedTx: typeof generateUnsignedTx;
+  setTxWitnessSet: typeof setTxWitnessSet;
+};
