35cd2d65b04e1bfebae3b18767f8fdc134b6cefd
Masood Abdolian <m.abdolian@gmail.com>
2024-06-15 11:57:05 +0330

    fix: Update xdefi wallet tests

--- wallets/xdefi-wallet/unitTests/src/utils.test.ts
+++ wallets/xdefi-wallet/unitTests/src/utils.test.ts
@@ -1,7 +1,7 @@
 import { Networks } from '@rosen-ui/constants';
 import { describe, expect, it } from 'vitest';
 
-import { generateOpReturnData } from '../../src/utils';
+import { generateOpReturnData } from '@rosen-network/bitcoin';
 
 describe('generateOpReturnData', () => {
   /**
