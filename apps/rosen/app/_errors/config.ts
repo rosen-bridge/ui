539498c9dfa353347afadbb958e8cc9929250360
Masood Abdolian <m.abdolian@gmail.com>
2024-07-03 10:41:47 +0000

    chore: Update error handler logic

--- apps/rosen/app/_errors/config.ts
+++ apps/rosen/app/_errors/config.ts
@@ -2,8 +2,4 @@ import { create } from './core';
 
 const KEY = '__TYPE__';
 
-export class MyError extends Error {
-  static [KEY] = 'my-error';
-}
-
-export const { wrap, unwrap } = create(KEY, [MyError]);
+export const { wrap, unwrap } = create(KEY, []);
