3855946cd8557a48b7a1fd39bda6ee18a61bb2af
Masood Abdolian <m.abdolian@gmail.com>
2024-08-21 06:17:17 +0000

    chore: Replace `getTokenMapObject` utility with `getRosenTokens` utility.

--- apps/rosen/app/_networks/getTokenMap.server.ts
+++ apps/rosen/app/_networks/getTokenMap.server.ts
@@ -1,15 +1,15 @@
 import { TokenMap } from '@rosen-bridge/tokens';
 
-import { getTokenMapObject } from './getTokenMapObject';
+import { getRosenTokens } from '@/_backend/utils';
 
 let tokenMap: TokenMap;
 
 /**
  * get a TokenMap instance using the Rosen TokenMapObject in the server.
  */
-export const getTokenMap = () => {
+export const getTokenMap = async () => {
   if (tokenMap) return tokenMap;
-  const tokens = getTokenMapObject();
+  const tokens = await getRosenTokens();
   tokenMap = new TokenMap(tokens);
   return tokenMap;
 };
