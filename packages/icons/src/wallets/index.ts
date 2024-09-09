3c938d55165e755ec42730c18605ec2e4964f693
Masood Abdolian <m.abdolian@gmail.com>
2024-09-09 11:50:21 +0000

    feat: Add Ethereum and MetaMask icons.

--- packages/icons/src/wallets/index.ts
+++ packages/icons/src/wallets/index.ts
@@ -2,6 +2,7 @@ export { ReactComponent as NamiIcon } from './nami.svg';
 export { ReactComponent as FlintIcon } from './flint.svg';
 export { ReactComponent as EternlIcon } from './eternl.svg';
 export { ReactComponent as LaceIcon } from './lace.svg';
+export { ReactComponent as MetaMaskIcon } from './metamask.svg';
 export { ReactComponent as NautilusIcon } from './nautilus.svg';
 export { ReactComponent as VesprIcon } from './vespr.svg';
 export { ReactComponent as XdefiIcon } from './xdefi.svg';
