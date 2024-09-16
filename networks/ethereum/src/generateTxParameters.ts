2299d02247bbf5d96095b68ecc0dc5673204e943
HFazelinia <hadifazelinia78@gmail.com>
2024-09-16 13:53:09 +0330

    integrate ethereum tx generation logic

---
+++ networks/ethereum/src/generateTxParameters.ts
@@ -0,0 +1,69 @@
+import { RosenAmountValue } from '@rosen-ui/types';
+import { TokenMap, RosenChainToken } from '@rosen-bridge/tokens';
+import { Networks } from '@rosen-ui/constants';
+import { ETH, transferABI } from './constants';
+import { Contract } from 'ethers';
+
+/**
+ * generates ethereum lock tx
+ * @param tokenId
+ * @param lockAddress
+ * @param fromAddress
+ * @param amount
+ * @param rosenData
+ * @returns
+ */
+export const generateTxParametersCore = async (
+  tokenId: string,
+  lockAddress: string,
+  fromAddress: string,
+  amount: RosenAmountValue,
+  rosenData: string
+) => {
+  let transactionParameters;
+  if (tokenId === ETH) {
+    transactionParameters = {
+      to: lockAddress,
+      from: fromAddress,
+      data: '0x' + rosenData,
+    };
+  } else {
+    const contract = new Contract(tokenId, transferABI, undefined);
+    const data = contract.interface.encodeFunctionData('transfer', [
+      lockAddress,
+      amount.toString(),
+    ]);
+
+    transactionParameters = {
+      to: tokenId,
+      from: fromAddress,
+      data: data + rosenData,
+    };
+  }
+
+  return transactionParameters;
+};
+
+export const generateTxParameters = (tokenMap: TokenMap) => {
+  return (
+    tokenId: string,
+    lockAddress: string,
+    fromAddress: string,
+    wrappedAmount: RosenAmountValue,
+    rosenData: string,
+    token: RosenChainToken
+  ) => {
+    const unwrappedAmount = tokenMap.unwrapAmount(
+      token[tokenMap.getIdKey(Networks.ETHEREUM)],
+      wrappedAmount,
+      Networks.ETHEREUM
+    ).amount;
+    return generateTxParametersCore(
+      tokenId,
+      lockAddress,
+      fromAddress,
+      unwrappedAmount,
+      rosenData
+    );
+  };
+};
--- networks/ethereum/src/utils.ts
+++ networks/ethereum/src/utils.ts
@@ -1,4 +1,45 @@
 import { isAddress, JsonRpcProvider } from 'ethers';
+import { SUPPORTED_CHAINS } from './constants';
+import { encodeAddress } from '@rosen-bridge/address-codec';
+
+/**
+ * generates metadata for lock transaction
+ * @param toChain
+ * @param toAddress
+ * @param fromAddress
+ * @param networkFee
+ * @param bridgeFee
+ * @returns
+ */
+export const generateLockData = async (
+  toChain: string,
+  toAddress: string,
+  networkFee: string,
+  bridgeFee: string
+): Promise<string> => {
+  // parse toChain
+  const toChainCode = SUPPORTED_CHAINS.indexOf(
+    toChain as (typeof SUPPORTED_CHAINS)[number]
+  );
+  if (toChainCode === -1) throw Error(`invalid toChain [${toChain}]`);
+  const toChainHex = toChainCode.toString(16).padStart(2, '0');
+
+  // parse bridgeFee
+  const bridgeFeeHex = BigInt(bridgeFee).toString(16).padStart(16, '0');
+
+  // parse networkFee
+  const networkFeeHex = BigInt(networkFee).toString(16).padStart(16, '0');
+
+  // parse toAddress
+  const addressHex = encodeAddress(toChain, toAddress);
+  const addressLengthCode = (addressHex.length / 2)
+    .toString(16)
+    .padStart(2, '0');
+
+  return Promise.resolve(
+    toChainHex + bridgeFeeHex + networkFeeHex + addressLengthCode + addressHex
+  );
+};
 
 /**
  * gets Ethereum current block height
