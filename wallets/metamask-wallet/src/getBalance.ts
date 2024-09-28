fd497d7beafdfc88c3731e53752588bb8327989e
Masood Abdolian <m.abdolian@gmail.com>
2024-09-28 13:41:31 +0000

    fix: Address the issue related to retrieving the token balance in Ethereum

--- wallets/metamask-wallet/src/getBalance.ts
+++ wallets/metamask-wallet/src/getBalance.ts
@@ -1,7 +1,9 @@
 import { RosenChainToken } from '@rosen-bridge/tokens';
 import { WalletCreatorConfig } from '@rosen-network/ethereum';
+import { tokenABI } from '@rosen-network/ethereum/dist/src/constants';
 import { NETWORKS } from '@rosen-ui/constants';
 import { RosenAmountValue } from '@rosen-ui/types';
+import { BrowserProvider, Contract } from 'ethers';
 
 import { getMetaMaskWallet } from './getMetaMaskWallet';
 
@@ -18,14 +20,30 @@ export const getBalanceCreator =
 
     if (!accounts?.length) return 0n;
 
-    const amount = await provider.request<string>({
-      method: 'eth_getBalance',
-      params: [accounts[0], 'latest'],
-    });
+    const tokenMap = await config.getTokenMap();
+
+    const tokenId = token[tokenMap.getIdKey(NETWORKS.ETHEREUM)];
+
+    let amount;
+
+    if (tokenId == 'eth') {
+      amount = await provider.request<string>({
+        method: 'eth_getBalance',
+        params: [accounts[0], 'latest'],
+      });
+    } else {
+      const browserProvider = new BrowserProvider(window.ethereum!);
+
+      const contract = new Contract(
+        tokenId,
+        tokenABI,
+        await browserProvider.getSigner()
+      );
+
+      amount = await contract.balanceOf(accounts[0]);
+    }
 
     if (!amount) return 0n;
-
-    const tokenMap = await config.getTokenMap();
 
     const wrappedAmount = tokenMap.wrapAmount(
       token[tokenMap.getIdKey(NETWORKS.ETHEREUM)],
