48f4d83388d00ffde13cc591f9165db34f88d64d
Masood Abdolian <m.abdolian@gmail.com>
2024-10-07 13:00:10 +0000

    chore: Update sidebar based on new AppBar changes

--- apps/rosen/app/SideBar.tsx
+++ apps/rosen/app/SideBar.tsx
@@ -8,49 +8,9 @@ import {
   Exchange,
   Headphones,
 } from '@rosen-bridge/icons';
-import {
-  AppBar,
-  AppLogo,
-  Grid,
-  NavigationButton,
-  Typography,
-} from '@rosen-bridge/ui-kit';
+import { AppBar, AppLogo } from '@rosen-bridge/ui-kit';
 
 import packageJson from '../package.json';
-
-const ROUTES = [
-  {
-    label: 'Bridge',
-    path: '/',
-    disabled: false,
-    icon: <Archway />,
-  },
-  {
-    label: 'Events',
-    path: '/events',
-    disabled: false,
-    icon: <Exchange />,
-  },
-  {
-    label: 'Assets',
-    path: '/assets',
-    disabled: false,
-    icon: <BitcoinCircle />,
-    badge: 'Beta',
-  },
-  {
-    label: 'Support',
-    path: '/support',
-    disabled: true,
-    icon: <Headphones />,
-  },
-  {
-    label: 'Dashboard',
-    path: '/dashboard',
-    disabled: true,
-    icon: <Dashboard />,
-  },
-];
 
 /**
  * render sidebar log and navigaiton buttons
@@ -61,40 +21,53 @@ export const SideBar = () => {
   const router = useRouter();
 
   return (
-    <AppBar>
-      <Link href="/">
-        <AppLogo darkLogoPath="/dark.png" lightLogoPath="/light.png"></AppLogo>
-      </Link>
-      <Grid
-        container
-        direction="column"
-        justifyContent="center"
-        alignItems="center"
-        spacing={1}
-        flexGrow={1}
-      >
-        {ROUTES.map((route) => (
-          <Grid key={route.label} item>
-            <NavigationButton
-              badge={route.badge}
-              disabled={route.disabled}
-              icon={route.icon}
-              isActive={pathname === route.path}
-              label={route.label}
-              onClick={() => router.push(route.path)}
-            />
-          </Grid>
-        ))}
-      </Grid>
-      <Grid item>
-        <Typography
-          textAlign="center"
-          variant="subtitle2"
-          color="textSecondary"
-        >
-          UI v{packageJson.version}
-        </Typography>
-      </Grid>
-    </AppBar>
+    <AppBar
+      logo={
+        <Link href="/">
+          <AppLogo darkLogoPath="/dark.png" lightLogoPath="/light.png" />
+        </Link>
+      }
+      versions={[
+        {
+          title: 'UI',
+          value: packageJson.version,
+        },
+      ]}
+      routes={[
+        {
+          label: 'Bridge',
+          path: '/',
+          disabled: false,
+          icon: <Archway />,
+        },
+        {
+          label: 'Events',
+          path: '/events',
+          disabled: false,
+          icon: <Exchange />,
+        },
+        {
+          label: 'Assets',
+          path: '/assets',
+          disabled: false,
+          icon: <BitcoinCircle />,
+          badge: 'Beta',
+        },
+        {
+          label: 'Support',
+          path: '/support',
+          disabled: true,
+          icon: <Headphones />,
+        },
+        {
+          label: 'Dashboard',
+          path: '/dashboard',
+          disabled: true,
+          icon: <Dashboard />,
+        },
+      ]}
+      isActive={(route) => pathname === route.path}
+      onNavigate={(route) => router.push(route.path)}
+    />
   );
 };
