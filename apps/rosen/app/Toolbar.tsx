1624a857ba4349dd11adc691b4bcdd425d57feff
Masood Abdolian <m.abdolian@gmail.com>
2024-10-12 12:15:37 +0330

    fix: Revert the isCentered property in the Rosen app toolbar

--- apps/rosen/app/Toolbar.tsx
+++ apps/rosen/app/Toolbar.tsx
@@ -24,6 +24,7 @@ const Toolbar = () => {
     <UiKitToolbar
       title={page ? pageTitleMap[page] ?? '' : ''}
       toolbarActions={<ToolbarThemeTogglerAction />}
+      isCentered
     />
   );
 };
