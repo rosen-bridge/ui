f4a773b702b83aa0bf2ef6e6abebafe42afeaa18
Masood Abdolian <m.abdolian@gmail.com>
2024-10-21 10:06:40 +0000

    fix: update pages

--- apps/rosen/app/failed/page.tsx
+++ apps/rosen/app/failed/page.tsx
@@ -2,6 +2,9 @@ export default async function Failed() {
   const response = await fetch(
     'https://jsonplaceholder.typicode.com/todos/fake',
   );
+  if (!response.ok) {
+    throw new Error('Network response was not ok ...............');
+  }
   const data: any = await response.json();
   return <div>failed page title: {data.title}</div>;
 }
--- apps/rosen/app/success/page.tsx
+++ apps/rosen/app/success/page.tsx
@@ -1,8 +1,5 @@
 export default async function Success() {
   const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-  if (!response.ok) {
-    throw new Error('Network response was not ok ...............');
-  }
   const data: any = await response.json();
   return <div>success page title: {data.title}</div>;
 }
