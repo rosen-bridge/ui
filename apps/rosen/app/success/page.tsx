3a5e621274700f4b1abb74263a808ee3bfe06a26
Masood Abdolian <m.abdolian@gmail.com>
2024-10-21 11:31:23 +0000

    chore: Update success page

--- apps/rosen/app/success/page.tsx
+++ apps/rosen/app/success/page.tsx
@@ -1,5 +1,29 @@
-export default async function Success() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-  const data: any = await response.json();
-  return <div>success page title: {data.title}</div>;
+'use client';
+
+import { useEffect, useState } from 'react';
+
+export default function Success() {
+  const [data, setData] = useState<any>({});
+  const [id, setId] = useState('');
+  async function load() {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/todos/${id}`,
+    );
+    if (!response.ok) {
+      throw new Error('THIS IS AN ERROR FROM CLIENT');
+    }
+    const data: any = await response.json();
+    setData(data);
+  }
+  function onInput(event: any) {
+    setId(event.target.value);
+  }
+  return (
+    <div>
+      success page title: {data.title}
+      <br />
+      <input value={id} onInput={onInput} />
+      <button onClick={load}>fetch</button>
+    </div>
+  );
 }
