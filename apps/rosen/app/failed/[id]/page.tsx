feaad00dd7bea0d0523d4c78f47f03bd006fb8e8
Masood Abdolian <m.abdolian@gmail.com>
2024-10-21 10:24:47 +0000

    chore: add dynamic id

--- apps/rosen/app/failed/[id]/page.tsx
+++ apps/rosen/app/failed/[id]/page.tsx
@@ -1,6 +1,7 @@
-export default async function Failed() {
+export default async function Failed({ params }: any) {
+  console.log(33333, params);
   const response = await fetch(
-    'https://jsonplaceholder.typicode.com/todos/fake',
+    `https://jsonplaceholder.typicode.com/todos/${params.id}`,
   );
   if (!response.ok) {
     throw new Error('Network response was not ok ...............');
