326745db90bd447bcb4ee0d972421c57313a8d76
ALI-HUP <ali.sheikhbahaie84@gmail.com>
2024-10-21 13:33:11 +0330

    feat: Added titles for both Guard and Watcher, set a dynamic title for Watcher to display the network, and updated Watcher’s layout.tsx

---
+++ .changeset/big-drinks-cough.md
@@ -0,0 +1,6 @@
+---
+'@rosen-bridge/watcher-app': minor
+'@rosen-bridge/guard-app': minor
+---
+
+Separated Watcher's layout.tsx into layout.tsx and App.tsx, added titles for both Guard and Watcher, and implemented a dynamic title for Watcher that displays the network
--- apps/watcher/app/layout.tsx
+++ apps/watcher/app/layout.tsx
@@ -1,72 +1,25 @@
 'use client';
 
-import React from 'react';
-import SWRConfig from '@rosen-ui/swr-mock';
-
-/**
- * FIXME: import NoSsr from ui-kit
- * local:ergo/rosen-bridge/ui#193
- */
-import { NoSsr } from '@mui/material';
-
-import {
-  AppSnackbar,
-  styled,
-  SnackbarProvider,
-  ThemeProvider,
-  CssBaseline,
-} from '@rosen-bridge/ui-kit';
-import { ApiKeyContextProvider } from '@rosen-bridge/shared-contexts';
-
-import SideBar from './SideBar';
-import Toolbar from './Toolbar';
-
-import { theme } from './_theme/theme';
-
-import mockedData from './_mock/mockedData';
-
-const Root = styled('div')(({ theme }) => ({
-  width: '100vw',
-  height: '100vh',
-  display: 'flex',
-  flexDirection: 'row',
-  backgroundColor: theme.palette.secondary.dark,
-  backgroundImage:
-    theme.palette.mode === 'light'
-      ? `linear-gradient(180deg, ${theme.palette.gradient.a} 0%, ${theme.palette.gradient.b} 20%, ${theme.palette.gradient.c} 40%, ${theme.palette.gradient.d} 60%, ${theme.palette.gradient.e} 80%, ${theme.palette.gradient.f} 100%)`
-      : 'none',
-  [theme.breakpoints.down('tablet')]: {
-    flexDirection: 'column',
-    backgroundImage:
-      theme.palette.mode === 'light'
-        ? `linear-gradient(90deg, ${theme.palette.gradient.a} 0%, ${theme.palette.gradient.b} 20%, ${theme.palette.gradient.c} 40%, ${theme.palette.gradient.d} 60%, ${theme.palette.gradient.e} 80%, ${theme.palette.gradient.f} 100%)`
-        : 'none',
-  },
-}));
-
-const Main = styled('main')(({ theme }) => ({
-  flexGrow: 1,
-  overflowY: 'auto',
-  minHeight: '100%',
-  backgroundColor: theme.palette.background.default,
-  borderTopLeftRadius: theme.shape.borderRadius * 2,
-  borderBottomLeftRadius: theme.shape.borderRadius * 2,
-  paddingTop: theme.shape.borderRadius,
-  paddingBottom: theme.shape.borderRadius * 3,
-  paddingLeft: theme.shape.borderRadius * 2,
-  paddingRight: theme.shape.borderRadius * 2,
-
-  [theme.breakpoints.down('tablet')]: {
-    backgroundColor: theme.palette.background.paper,
-    borderTopRightRadius: theme.shape.borderRadius * 2,
-    borderBottomLeftRadius: 0,
-    paddingLeft: theme.spacing(2),
-    paddingRight: theme.spacing(2),
-    paddingBottom: theme.shape.borderRadius * 6,
-  },
-}));
+import React, { useEffect } from 'react';
+import App from './App';
+import useInfo from './_hooks/useInfo';
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  const { data: info, isLoading } = useInfo();
+
+  useEffect(() => {
+    const capitalizeFirstLetter = (network: string) =>
+      network.charAt(0).toUpperCase() + network.slice(1);
+
+    const networkTitle = isLoading
+      ? 'Rosen Watcher'
+      : `${
+          info?.network ? capitalizeFirstLetter(info.network) : ''
+        } Rosen Watcher`;
+
+    document.title = networkTitle;
+  }, [isLoading, info]);
+
   return (
     /**
      * TODO: get `lang` from url language path segment
@@ -74,32 +27,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
      */
     <html lang="en">
       <body>
-        <NoSsr>
-          <ThemeProvider theme={theme}>
-            <>
-              <CssBaseline />
-              <SnackbarProvider>
-                <ApiKeyContextProvider>
-                  <Root>
-                    <SideBar />
-                    <SWRConfig
-                      useMockedApis={
-                        process.env.NEXT_PUBLIC_USE_MOCKED_APIS === 'true'
-                      }
-                      fakeData={mockedData}
-                    >
-                      <Main>
-                        <Toolbar />
-                        {children}
-                        <AppSnackbar />
-                      </Main>
-                    </SWRConfig>
-                  </Root>
-                </ApiKeyContextProvider>
-              </SnackbarProvider>
-            </>
-          </ThemeProvider>
-        </NoSsr>
+        <App>{children}</App>
       </body>
     </html>
   );
