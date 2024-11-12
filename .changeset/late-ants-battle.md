5b18a00bfd0d0789585282e5dfb3631688d55a6d
fateme.r <fatemeh.r3366@gmail.com>
2024-11-12 11:27:33 +0330

    update rosen-app packages

---
+++ .changeset/late-ants-battle.md
@@ -0,0 +1,5 @@
+---
+'@rosen-bridge/rosen-app': patch
+---
+
+Update scanner packages
--- apps/rosen-service/src/configs.ts
+++ apps/rosen-service/src/configs.ts
@@ -59,6 +59,7 @@ const getConfig = () => {
         tokens: {
           rwt: nodeConfig.get<string>('bitcoin.tokens.rwt'),
         },
+        esploraUrl: nodeConfig.get<string>('bitcoin.esplora.url'),
         rpcUrl: nodeConfig.get<string>('bitcoin.rpc.url'),
         rpcUsername: getOptionalString('bitcoin.rpc.username'),
         rpcPassword: getOptionalString('bitcoin.rpc.password'),
