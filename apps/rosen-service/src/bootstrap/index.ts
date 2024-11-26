7c23492eae828cf7b308083759f12014c244ce29
Masood Abdolian <m.abdolian@gmail.com>
2024-11-26 13:26:28 +0000

    fix: Resolve the bug related to the Rosen service logger that was caused by the Prettier import order plugin

--- apps/rosen-service/src/bootstrap/index.ts
+++ apps/rosen-service/src/bootstrap/index.ts
@@ -2,5 +2,5 @@
  * CAUTION: Do not move this import. It is needed for logger of other modules to
  * work
  */
-import './register-signal-handlers';
 import './winston';
+import './register-signal-handlers';
