5b9798620d2f5f8bac2ab3733ae8e7f12bfc3aea
saeedehTorkamani <saeedehtorkamani@gmail.com>
2025-03-17 06:37:49 +0000

    Fix : Improve 2 conditions and , fix conflict

--- apps/rosen/app/(bridge)/BridgeTransaction.tsx
+++ apps/rosen/app/(bridge)/BridgeTransaction.tsx
@@ -10,6 +10,7 @@ import {
   Tooltip,
   Typography,
 } from '@rosen-bridge/ui-kit';
+import { NETWORKS } from '@rosen-ui/constants';
 
 import {
   useNetwork,
@@ -105,32 +106,43 @@ export const BridgeTransaction = ({
             padding: (theme) => theme.spacing(3),
           }}
         >
+          {sourceValue == NETWORKS.bitcoin.key && (
+            <Alert severity="warning" icon={false}>
+              We only support native SegWit addresses (P2WPKH or P2WSH) for the
+              source address.
+            </Alert>
+          )}
           <div style={{ flexGrow: '1' }} />
           <Amount
             title="Transaction Fee"
-            value={networkFeeRaw}
+            value={!tokenValue ? undefined : networkFeeRaw}
             unit={tokenInfo?.tokenName}
             loading={isPending}
           />
           <Amount
             title="Bridge Fee"
-            value={bridgeFeeRaw}
+            value={!tokenValue ? undefined : bridgeFeeRaw}
             unit={tokenInfo?.tokenName}
             loading={isPending}
           />
           <Amount
             title="Min Transfer"
-            value={minTransferRaw}
+            value={!tokenValue ? undefined : minTransferRaw}
             unit={tokenInfo?.tokenName}
             loading={isPending}
           />
           <Divider sx={{ borderStyle: 'dashed' }} />
           <Amount
             title="You Will Receive"
-            value={receivingAmountRaw}
+            value={
+              !tokenValue || receivingAmountRaw === '0'
+                ? undefined
+                : receivingAmountRaw
+            }
             unit={targetTokenInfo?.name}
             loading={isPending}
           />
+
           {!!error && (
             <Alert
               severity="error"
--- packages/ui-kit/src/components/common/Amount.tsx
+++ packages/ui-kit/src/components/common/Amount.tsx
@@ -1,12 +1,11 @@
 import { Box, Typography } from '../base';
 
 export interface AmountProps {
-  value: string;
+  value?: bigint | number | string;
   loading?: boolean;
   size?: 'normal' | 'large';
   title?: string;
   unit?: string;
-  justifyContent?: 'flex-start' | 'center' | 'space-between';
 }
 
 /**
@@ -18,80 +17,99 @@ export const Amount = ({
   size = 'normal',
   title,
   unit,
-  justifyContent,
 }: AmountProps) => {
-  const [number, decimals] = value.split('.');
+  let number: string | undefined;
+
+  let decimals: string | undefined;
+
+  switch (typeof value) {
+    case 'bigint': {
+      decimals = value.toLocaleString();
+      break;
+    }
+
+    case 'number': {
+      const sections = value
+        .toLocaleString('fullwide', {
+          minimumFractionDigits: 1,
+          maximumFractionDigits: 20,
+        })
+        .split('.');
+
+      number = sections.at(0);
+
+      decimals = sections.at(1) || '0';
+
+      break;
+    }
+
+    case 'string': {
+      const sections = value.split('.');
+
+      number = sections.at(0)!.toLocaleString();
+
+      decimals = sections.at(1);
+
+      break;
+    }
+  }
+
   return (
     <Box
       sx={{
-        minWidth: '70px',
-        display: 'flex',
+        display: title ? 'flex' : 'inline-flex',
         alignItems: 'center',
-        justifyContent: justifyContent ?? 'space-between',
       }}
     >
       {!!title && (
         <Typography
           color="text.secondary"
-          sx={{ fontSize: size == 'normal' ? '0.75rem' : '1rem' }}
+          sx={{
+            flexGrow: 1,
+            fontSize: size == 'normal' ? '0.75rem' : '1rem',
+          }}
         >
           {title}
         </Typography>
       )}
-      <Box sx={{ display: 'flex', alignItems: 'baseline' }}>
-        {loading && (
-          <Typography
-            color="text.primary"
-            sx={{ fontSize: size == 'normal' ? '1rem' : '1.5rem' }}
-          >
-            Pending...
-          </Typography>
-        )}
-        {!loading && (
-          <>
-            <Typography
-              color="text.primary"
-              sx={{ fontSize: size == 'normal' ? '0.75rem' : '1.375rem' }}
-            >
-              {value === '0'
-                ? '0'
-                : +value
-                  ? number.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-                  : '-'}
-            </Typography>
-            {!!+value && (
-              <>
-                {!!decimals && (
-                  <Typography
-                    color="text.secondary"
-                    sx={{
-                      fontSize:
-                        size == 'normal'
-                          ? 'calc(0.75rem * 0.75)'
-                          : 'calc(1.5rem * 0.75)',
-                    }}
-                  >
-                    .{decimals}
-                  </Typography>
-                )}
-                {!!unit && (
-                  <Typography
-                    color="text.secondary"
-                    sx={{
-                      fontSize:
-                        size == 'normal'
-                          ? 'calc(0.75rem * 0.75)'
-                          : 'calc(1.5rem * 0.75)',
-                    }}
-                  >
-                    &nbsp;{unit}
-                  </Typography>
-                )}
-              </>
-            )}
-          </>
-        )}
-      </Box>
+      {loading && (
+        <Typography
+          color="text.primary"
+          sx={{ fontSize: size == 'normal' ? '1rem' : '1.5rem' }}
+        >
+          Pending...
+        </Typography>
+      )}
+      {!loading && (
+        <Typography
+          color="text.primary"
+          sx={{ fontSize: size == 'normal' ? '0.75rem' : '1.375rem' }}
+        >
+          {number || '-'}
+        </Typography>
+      )}
+      {!loading && !!decimals && (
+        <Typography
+          color="text.secondary"
+          sx={{
+            fontSize:
+              size == 'normal' ? 'calc(0.75rem * 0.75)' : 'calc(1.5rem * 0.75)',
+          }}
+        >
+          .{decimals}
+        </Typography>
+      )}
+      {!loading && !!unit && !!number && (
+        <Typography
+          color="text.secondary"
+          sx={{
+            fontSize:
+              size == 'normal' ? 'calc(0.75rem * 0.75)' : 'calc(1.5rem * 0.75)',
+          }}
+        >
+          &nbsp;{unit}
+        </Typography>
+      )}
     </Box>
   );
 };
