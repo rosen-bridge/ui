f96d6a0477fa4bdf79b445a55870c32e19139588
Masood Abdolian <m.abdolian@gmail.com>
2025-04-30 08:54:31 +0000

    support get balance

--- wallets/wallet-connect/src/evm/wallet.ts
+++ wallets/wallet-connect/src/evm/wallet.ts
@@ -1,5 +1,4 @@
 import { RosenChainToken } from '@rosen-bridge/tokens';
-import { EvmChains } from '@rosen-network/evm';
 import { NETWORKS } from '@rosen-ui/constants';
 import { Network } from '@rosen-ui/types';
 import {
@@ -38,6 +37,8 @@ export abstract class WalletConnectEVM extends WalletConnect {
           'eth_sign',
           'personal_sign',
           'eth_signTypedData',
+          'eth_getBalance',
+          'eth_call',
         ],
       },
     };
@@ -66,11 +67,40 @@ export abstract class WalletConnectEVM extends WalletConnect {
 
     const tokenMap = await this.config.getTokenMap();
 
-    let amount = await this.config.getBalance(
-      this.currentChain as EvmChains,
-      address,
-      token,
-    );
+    // let amount = await this.config.getBalance(
+    //   this.currentChain as EvmChains,
+    //   address,
+    //   token,
+    // );
+
+    let request;
+
+    if (token.type === 'native') {
+      request = {
+        method: 'eth_getBalance',
+        params: [address, 'latest'],
+      };
+    } else {
+      // Encode the data for balanceOf(address)
+      const data = '0x70a08231' + address.replace('0x', '').padStart(64, '0'); // pad to 32 bytes
+
+      request = {
+        method: 'eth_call',
+        params: [
+          {
+            to: token.tokenId,
+            data,
+          },
+          'latest',
+        ],
+      };
+    }
+
+    const amount = await this.signClient.request<string>({
+      topic: this.session!.topic,
+      chainId: this.chainId,
+      request,
+    });
 
     if (!amount) return 0n;
 
