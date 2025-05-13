4c7fdae11df258e8cf442fae06f83e621b4a8e1b
Masood Abdolian <m.abdolian@gmail.com>
2025-05-13 08:52:11 +0000

    feat: Add empty slots according to the period to display all weeks, months, or years, even if they contain no data

--- apps/guard/app/(dashboard)/@revenue/RevenueChart.tsx
+++ apps/guard/app/(dashboard)/@revenue/RevenueChart.tsx
@@ -3,7 +3,7 @@ import Chart from 'react-apexcharts';
 
 import { useTheme } from '@rosen-bridge/ui-kit';
 import { ChartPeriod } from '@rosen-ui/types';
-import { getDecimalString, roundToPrecision } from '@rosen-ui/utils';
+import { getDecimalString } from '@rosen-ui/utils';
 import moment from 'moment';
 
 import { ApiRevenueChartResponse } from '@/_types/api';
@@ -74,14 +74,91 @@ export const RevenueChart = ({ period, data }: RevenueChartProps) => {
       .sort((a, b) => a - b);
   }, [data]);
 
+  const incrementDate = (date: number, period: ChartPeriod): Date => {
+    const result = new Date(date);
+
+    switch (period) {
+      case 'week':
+        result.setUTCDate(result.getUTCDate() + 7);
+        break;
+      case 'month':
+        result.setUTCMonth(result.getUTCMonth() + 1);
+        break;
+      case 'year':
+        result.setUTCFullYear(result.getUTCFullYear() + 1);
+        break;
+    }
+
+    return result;
+  };
+
+  const normalizeDate = (timestamp: number, period: ChartPeriod): Date => {
+    const result = new Date(timestamp);
+
+    switch (period) {
+      case 'week': {
+        const day = result.getUTCDay();
+        const diff = (day === 0 ? -6 : 1) - day;
+        result.setUTCDate(result.getUTCDate() + diff);
+        break;
+      }
+      case 'month':
+        result.setUTCDate(1);
+        break;
+      case 'year':
+        result.setUTCMonth(0, 1);
+        break;
+    }
+
+    result.setUTCHours(0, 0, 0, 0);
+
+    return result;
+  };
+
+  const slotsInPeriod = useMemo(() => {
+    if (slots.length === 0) return {};
+
+    const min = slots[0];
+    const max = slots[slots.length - 1];
+
+    const start = normalizeDate(min, period).getTime();
+    const end = normalizeDate(max, period).getTime();
+
+    const groups = {} as { [key: number]: number[] };
+
+    for (const slot of slots) {
+      const key = normalizeDate(slot, period).getTime();
+
+      groups[key] ||= [];
+
+      groups[key].push(slot);
+    }
+
+    let current = start;
+
+    while (current <= end) {
+      groups[current] ||= [];
+      current = incrementDate(current, period).getTime();
+    }
+
+    return groups;
+  }, [period, slots]);
+
+  const slotsInPeriodKeys = useMemo(() => {
+    return Object.keys(slotsInPeriod)
+      .map(Number)
+      .sort((a, b) => a - b);
+  }, [slotsInPeriod]);
+
   const apexChartOptions = useMemo(
     () => ({
       ...baseChartOptions,
       xaxis: {
         ...baseChartOptions.xaxis,
         categories:
-          slots.map((slot) => moment(+slot).format(getDateFormat(period))) ??
-          [],
+          slotsInPeriodKeys.map((slot) =>
+            moment(+slot).format(getDateFormat(period)),
+          ) ?? [],
       },
       theme: {
         mode: theme.palette.mode,
@@ -105,23 +182,22 @@ export const RevenueChart = ({ period, data }: RevenueChartProps) => {
               theme.palette.error.light,
             ],
     }),
-    [period, slots, theme],
+    [period, slotsInPeriodKeys, theme],
   );
 
   const apexChartSeries = useMemo(
     () =>
       data.map((token) => ({
         name: token.title.name,
-        data: slots.map(
-          (slot) =>
-            +getDecimalString(
-              token.data.find((item) => item.label == slot.toString())
-                ?.amount || '0',
-              token.title.decimals,
-            ),
-        ),
+        data: slotsInPeriodKeys.map((key) => {
+          const amount = token.data
+            .filter((item) => slotsInPeriod[key].includes(+item.label))
+            .reduce((sum, item) => sum + Number(item.amount), 0);
+
+          return +getDecimalString(amount.toString(), token.title.decimals);
+        }),
       })),
-    [data, slots],
+    [data, slotsInPeriod, slotsInPeriodKeys],
   );
 
   return (
