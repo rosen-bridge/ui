cf0acef56b96d5b491909c4dacdfcb4a352b8563
Mojtaba Fazelinia <mfazelinia@gmail.com>
2025-05-17 16:52:38 +0330

    Fix bridge background

--- apps/rosen/app/(bridge)/layout.tsx
+++ apps/rosen/app/(bridge)/layout.tsx
@@ -2,13 +2,45 @@
 
 import { Fragment, PropsWithChildren } from 'react';
 
-import { PageHeading } from '@rosen-bridge/ui-kit';
+import { PageHeading, styled } from '@rosen-bridge/ui-kit';
+
+import { CubeNetSvg } from './CubeNet';
+
+const Background = styled('div')(({ theme }) => ({
+  'position': 'absolute',
+  'inset': 0,
+  'backgroundImage':
+    theme.palette.mode === 'light'
+      ? `linear-gradient(180deg,${theme.palette.primary.light},${theme.palette.background.default},${theme.palette.background.default},${theme.palette.secondary.light})`
+      : 'none',
+  'zIndex': '0',
+  '& > svg': {
+    'position': 'absolute',
+    'width': '100%',
+    'zIndex': '-1',
+    'left': 0,
+    '&.top': {
+      top: 0,
+    },
+    '&.bottom': {
+      bottom: 0,
+      transform: 'rotate(180deg)',
+    },
+  },
+  [theme.breakpoints.down('tablet')]: {
+    display: 'none',
+  },
+}));
 
 const PageLayout = ({ children }: PropsWithChildren) => {
   return (
     <Fragment>
       <PageHeading title="Rosen Bridge" />
       {children}
+      <Background>
+        <CubeNetSvg color="primary" className="top" />
+        <CubeNetSvg color="secondary" className="bottom" />
+      </Background>
     </Fragment>
   );
 };
--- apps/rosen/app/(bridge)/page.tsx
+++ apps/rosen/app/(bridge)/page.tsx
@@ -18,33 +18,6 @@ import {
 import { BridgeForm } from './BridgeForm';
 import { BridgeTransaction } from './BridgeTransaction';
 import { ConnectOrSubmitButton } from './ConnectOrSubmitButton';
-import { CubeNetSvg } from './cube-net';
-
-const Background = styled('div')(({ theme }) => ({
-  'position': 'absolute',
-  'inset': 0,
-  'backgroundImage':
-    theme.palette.mode === 'light'
-      ? `linear-gradient(180deg,${theme.palette.primary.light},${theme.palette.background.default},${theme.palette.background.default},${theme.palette.secondary.light})`
-      : 'none',
-  'zIndex': '0',
-  '& > svg': {
-    'position': 'absolute',
-    'width': '100%',
-    'zIndex': '-1',
-    'left': 0,
-    '&.top': {
-      top: 0,
-    },
-    '&.bottom': {
-      bottom: 0,
-      transform: 'rotate(180deg)',
-    },
-  },
-  [theme.breakpoints.down('tablet')]: {
-    display: 'none',
-  },
-}));
 
 const BridgeContainer = styled('div')(({ theme }) => ({
   display: 'flex',
@@ -100,10 +73,6 @@ const RosenBridge = () => {
 
   return (
     <>
-      <Background>
-        <CubeNetSvg color="primary" className="top" />
-        <CubeNetSvg color="secondary" className="bottom" />
-      </Background>
       <FormProvider {...methods}>
         <NetworkProvider>
           <WalletProvider>
