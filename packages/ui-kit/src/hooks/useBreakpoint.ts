663b282695a4c87974234871b15a4914a3126aa6
Masood Abdolian <m.abdolian@gmail.com>
2025-06-25 13:10:32 +0000

    feat: Implement a custom hook to handle size changes within JavaScript logic

---
+++ packages/ui-kit/src/hooks/useBreakpoint.ts
@@ -0,0 +1,37 @@
+import { Breakpoint } from '@mui/material';
+
+import { useMediaQuery } from './useMediaQuery';
+import { useTheme } from './useTheme';
+
+export const useBreakpoint = (
+  breakpoint:
+    | Breakpoint
+    | `${Breakpoint}-${'up' | 'down' | 'not'}`
+    | `${Breakpoint}-to-${Breakpoint}`,
+) => {
+  const theme = useTheme();
+
+  const keys = breakpoint.split('-');
+
+  const args: string[] = [];
+
+  args.push(keys[0]);
+
+  if (keys[2]) {
+    args.push(keys[2]);
+  }
+
+  let action = keys[1] == 'to' ? 'between' : keys[1] || 'only';
+
+  if (breakpoint == 'mobile-down' || breakpoint == 'desktop-up') {
+    action = 'only';
+  }
+
+  const func = theme.breakpoints[action as keyof typeof theme.breakpoints] as (
+    ...args: string[]
+  ) => string;
+
+  const query = func(...args);
+
+  return useMediaQuery(query);
+};
