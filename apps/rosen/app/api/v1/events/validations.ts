9803ee5cc1a9349384a165a99437bd00e38af3ef
Masood Abdolian <m.abdolian@gmail.com>
2025-07-07 13:37:13 +0000

    feat: Improve the Events API context by adding support for filters

--- apps/rosen/app/api/v1/events/validations.ts
+++ apps/rosen/app/api/v1/events/validations.ts
@@ -1,20 +1,14 @@
 import { NextRequest } from 'next/server';
 
-import Joi from 'joi';
-
-interface GETQueryParams {
-  offset: number;
-  limit: number;
-}
-const getQueryParamsSchema = Joi.object<GETQueryParams>().keys({
-  offset: Joi.number().min(0).default(0),
-  limit: Joi.number().min(0).max(100).default(20),
-});
+import {
+  FiltersSchema,
+  searchParamsToFilters,
+} from '@rosen-bridge/ui-kit/dist/components/common/smartSearch/server';
 
 /**
  * validate get requests
  * @param request
  */
 export const validateGet = ({ nextUrl: { searchParams } }: NextRequest) => {
-  return getQueryParamsSchema.validate(Object.fromEntries(searchParams));
+  return FiltersSchema.validate(searchParamsToFilters(searchParams));
 };
