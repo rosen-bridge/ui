547dbe65928095450c9bca8994976f75e0af03e6
Mohsen Javidpanah <moshen212@gmail.com>
2025-07-12 10:54:09 +0330

    Updated the configs utils

--- apps/rosen-service2/src/configs/utils.ts
+++ apps/rosen-service2/src/configs/utils.ts
@@ -1,4 +1,5 @@
 import { ConfigValidator } from '@rosen-bridge/config';
+import { NETWORKS } from '@rosen-ui/constants';
 import config from 'config';
 import * as fs from 'fs';
 import JsonBigIntFactory from 'json-bigint';
@@ -9,7 +10,6 @@ import { SUPPORTED_CHAINS } from '../constants';
 import { Configs } from '../types';
 import { ChainConfigsReader } from './chains';
 
-const ERGO_CHAIN = 'ergo';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const JsonBigInt = JsonBigIntFactory({
   alwaysParseAsBig: false,
@@ -34,7 +34,7 @@ export const validateConfigs = (): Configs => {
   // TODO: Must remove after adding the Ergo config segment
   configs.chains['ergo'] = {};
   for (const chain of SUPPORTED_CHAINS) {
-    if (chain == ERGO_CHAIN || configs.chains[chain].active) {
+    if (chain == NETWORKS.ergo.key || configs.chains[chain].active) {
       const chainConfigReader = new ChainConfigsReader(chain);
       configs.chains[chain].contracts = chainConfigReader.data;
     }
