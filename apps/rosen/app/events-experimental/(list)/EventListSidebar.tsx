0e36a52dba273e71ac3a66e8ce131658dadf75fd
Masood Abdolian <m.abdolian@gmail.com>
2025-07-19 14:15:47 +0000

    add columns

--- apps/rosen/app/events-experimental/(list)/EventListSidebar.tsx
+++ apps/rosen/app/events-experimental/(list)/EventListSidebar.tsx
@@ -1,3 +1,5 @@
+import { ReactNode } from 'react';
+
 import { AngleRight, Exchange } from '@rosen-bridge/icons';
 import {
   Card,
@@ -25,104 +27,103 @@ import { getDecimalString } from '@rosen-ui/utils';
 
 import { EventItem } from '@/_types';
 
+// TODO: move this component to the ui-kit
+const Columns = ({
+  children,
+  gap,
+  rule,
+  width,
+}: {
+  children?: ReactNode;
+  gap?: string;
+  rule?: boolean;
+  width: string;
+}) => {
+  return (
+    <div
+      style={{
+        columnWidth: width,
+        columnGap: gap,
+        columnRule: rule ? '1px solid red' : '',
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
 const Content = ({ value }: EventListSidebarProps) => {
   if (!value) return <EmptyState />;
 
   return (
-    <Box
-      sx={{
-        'containerType': 'inline-size',
-        'display': 'flex',
-        'flexDirection': 'row',
-        'flexWrap': 'wrap',
-        'columnGap': (theme) => theme.spacing(2),
-        '& > div': {
-          flexGrow: 1,
-          width: 0,
-        },
-        '@container (max-width: 600px)': {
-          'flexDirection': 'column',
-          '& > div': {
-            width: '100%',
-          },
-          '.MuiDivider-root': {
-            display: 'none',
-          },
-        },
-      }}
-    >
-      <div>
-        <Label orientation={'horizontal'} label="Event Id">
-          <Identifier copyable value={value.eventId} />
-        </Label>
-        <Label label="Status">
-          <Chip
-            label={value.status && 'completed'}
-            color={'success'}
-            icon={'CheckCircle'}
-          />
-        </Label>
-        <Label label="Token">
-          <Token reverse name={value.lockToken.name} />
-        </Label>
-        <Label label="Amount">
-          <Amount2
-            value={getDecimalString(
-              value.amount,
-              value.lockToken.significantDecimals,
-            )}
-          />
-        </Label>
-        <Label label="Chain"></Label>
-        <Label label="From" inset>
-          <Network name={value.fromChain} reverse />
-        </Label>
-        <Label label="To" inset>
-          <Network name={value.toChain} reverse />
-        </Label>
-        <Label label="Tx IDs"></Label>
-        <Label label="Source Tx" inset>
-          <Identifier copyable value={value.sourceTxId} />
-        </Label>
-        <Label label="Payment Tx" inset>
-          <Identifier copyable value={value.paymentTxId || ''} />
-        </Label>
-        <Label label="Reward Tx" inset>
-          <Identifier copyable value={value.spendTxId || ''} />
-        </Label>
-      </div>
-      <Divider orientation="vertical" flexItem sx={{ borderStyle: 'dashed' }} />
-      <div>
-        <Label label="Address"></Label>
-        <Label label="From" inset>
-          <Identifier copyable value={value.fromAddress} />
-        </Label>
-        <Label label="To" inset>
-          <Identifier copyable value={value.toAddress} />
-        </Label>
-        <Label label="Duration">
-          <RelativeTime timestamp={value.timestamp} />
-        </Label>
-        <Label label="Bridge fee">
-          <Amount2
-            value={getDecimalString(
-              value.bridgeFee,
-              value.lockToken.significantDecimals,
-            )}
-          />
-        </Label>
-        <Label label="Network Fee">
-          <Amount2
-            value={getDecimalString(
-              value.networkFee,
-              value.lockToken.significantDecimals,
-            )}
-          />
-        </Label>
-        <Label label="Reports">{value.WIDsCount}</Label>
-        <Label label="Height">{value.height}</Label>
-      </div>
-    </Box>
+    <Columns width="20rem" rule>
+      <Label orientation={'horizontal'} label="Event Id">
+        <Identifier copyable value={value.eventId} />
+      </Label>
+      <Label label="Status">
+        <Chip
+          label={value.status && 'completed'}
+          color={'success'}
+          icon={'CheckCircle'}
+        />
+      </Label>
+      <Label label="Token">
+        <Token reverse name={value.lockToken.name} />
+      </Label>
+      <Label label="Amount">
+        <Amount2
+          value={getDecimalString(
+            value.amount,
+            value.lockToken.significantDecimals,
+          )}
+        />
+      </Label>
+      <Label label="Chain"></Label>
+      <Label label="From" inset>
+        <Network name={value.fromChain} reverse />
+      </Label>
+      <Label label="To" inset>
+        <Network name={value.toChain} reverse />
+      </Label>
+      <Label label="Tx IDs"></Label>
+      <Label label="Source Tx" inset>
+        <Identifier copyable value={value.sourceTxId} />
+      </Label>
+      <Label label="Payment Tx" inset>
+        <Identifier copyable value={value.paymentTxId || ''} />
+      </Label>
+      <Label label="Reward Tx" inset>
+        <Identifier copyable value={value.spendTxId || ''} />
+      </Label>
+      <Label label="Address"></Label>
+      <Label label="From" inset>
+        <Identifier copyable value={value.fromAddress} />
+      </Label>
+      <Label label="To" inset>
+        <Identifier copyable value={value.toAddress} />
+      </Label>
+      <Label label="Duration">
+        <RelativeTime timestamp={value.timestamp} />
+      </Label>
+      <Label label="Bridge fee">
+        <Amount2
+          value={getDecimalString(
+            value.bridgeFee,
+            value.lockToken.significantDecimals,
+          )}
+        />
+      </Label>
+      <Label label="Network Fee">
+        <Amount2
+          value={getDecimalString(
+            value.networkFee,
+            value.lockToken.significantDecimals,
+          )}
+        />
+      </Label>
+      <Label label="Reports">{value.WIDsCount}</Label>
+      <Label label="Height">{value.height}</Label>
+    </Columns>
   );
 };
 
