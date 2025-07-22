da7027b567ee1f62af752980f77a5b8344a37b6b
hazhirmohammadi <hazhir6690@gmail.com>
2025-07-22 19:04:31 +0330

    fix event details page

--- apps/rosen/src/app/events-experimental/[details]/layout.tsx
+++ apps/rosen/src/app/events-experimental/[details]/layout.tsx
@@ -5,12 +5,7 @@ import { Fragment, PropsWithChildren } from 'react';
 import { PageHeading } from '@rosen-bridge/ui-kit';
 
 const PageLayout = ({ children }: PropsWithChildren) => {
-  return (
-    <Fragment>
-      <PageHeading title="Events Details" />
-      {children}
-    </Fragment>
-  );
+  return <Fragment>{children}</Fragment>;
 };
 
 export default PageLayout;
--- apps/rosen/src/app/events-experimental/[details]/page.tsx
+++ apps/rosen/src/app/events-experimental/[details]/page.tsx
@@ -1,12 +1,19 @@
 'use client';
 
-import { Center } from '@rosen-bridge/ui-kit';
+import Link from 'next/link';
+
+import { Button, Center, Stack } from '@rosen-bridge/ui-kit';
 import { UnderDevelop } from '@rosen-bridge/ui-kit';
 
 const Page = () => {
   return (
     <Center style={{ minHeight: 'calc(100vh - 224px)' }}>
-      <UnderDevelop />
+      <Stack spacing={4} alignItems="center">
+        <UnderDevelop />
+        <Link href="/">
+          <Button variant="contained">Go Home</Button>
+        </Link>
+      </Stack>
     </Center>
   );
 };
