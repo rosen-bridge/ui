bc4ada140b80f1983a0b1103c6d4503e55334026
Masood Abdolian <m.abdolian@gmail.com>
2025-08-27 15:06:39 +0000

    fix: Upgrade the Address section in the Watcher app to use the reusable component

---
+++ .changeset/dark-cooks-sit.md
@@ -0,0 +1,5 @@
+---
+'@rosen-bridge/watcher-app': patch
+---
+
+Redesigned the Address section using the new Identifier component from the UI kit, replacing the old buttons and card for a cleaner, more consistent, and responsive experience across all devices
--- apps/watcher/app/(home)/@address/page.tsx
+++ apps/watcher/app/(home)/@address/page.tsx
@@ -2,48 +2,22 @@
 
 import React from 'react';
 
-import { Box, Card, FullCard, Grid, Typography } from '@rosen-bridge/ui-kit';
+import { FullCard, Identifier } from '@rosen-bridge/ui-kit';
 import { fetcher } from '@rosen-ui/swr-helpers';
 import useSWR from 'swr';
 
 import { ApiInfoResponse } from '@/_types/api';
 
-import { AddressSkeleton } from './AddressSkeleton';
-import { CopyButton } from './CopyButton';
-import { QrCodeButton } from './QrCodeButton';
-
 const Address = () => {
   const { data, isLoading } = useSWR<ApiInfoResponse>('/info', fetcher);
-
   return (
     <FullCard title="Address">
-      {isLoading ? (
-        <AddressSkeleton />
-      ) : (
-        data && (
-          <Card variant="outlined" sx={{ p: 1, mx: 1 }}>
-            <Grid container justifyContent="space-between">
-              <Grid item>
-                <Typography>{data.address}</Typography>
-              </Grid>
-              <Grid item>
-                <Box
-                  sx={{
-                    m: -1,
-                    ml: 'auto',
-                    flexShrink: 0,
-                    bgcolor: (theme) => theme.palette.divider,
-                    borderLeft: (theme) => `1px solid ${theme.palette.divider}`,
-                  }}
-                >
-                  <CopyButton address={data.address} />
-                  <QrCodeButton address={data.address} />
-                </Box>
-              </Grid>
-            </Grid>
-          </Card>
-        )
-      )}
+      <Identifier
+        copyable
+        qrcode
+        value={data?.address || ''}
+        loading={isLoading}
+      />
     </FullCard>
   );
 };
