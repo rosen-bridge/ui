74806b672f579ff29dd015defe1799d679db8497
hazhirmohammadi <hazhir6690@gmail.com>
2025-09-10 13:23:54 +0330

    feat: update branch

--- apps/guard/app/(dashboard)/@health/page.tsx
+++ apps/guard/app/(dashboard)/@health/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import { fetcher } from '@rosen-ui/swr-helpers';
 import useSWR from 'swr';
@@ -11,15 +11,11 @@ import { ApiInfoResponse } from '@/_types/api';
 const Health = () => {
   const { data, isLoading, error } = useSWR<ApiInfoResponse>('/info', fetcher);
 
-  // useEffect(() => {
-  //   console.log(data?.health);
-  // }, [data]);
-
   return (
     <Status
       isLoading={isLoading}
       variant={(data?.health.status.toLowerCase() as StatusType) ?? 'broken'}
-      details={'hi from details tooltip'}
+      details={data?.health.trialErrors.join('\n')}
     />
   );
 };
