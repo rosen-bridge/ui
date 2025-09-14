a368dec020405a0b3460bf3289e052e481a757fd
hazhirmohammadi <hazhir6690@gmail.com>
2025-09-14 09:42:54 +0330

    refactor the SourceTx copy btn

--- apps/rosen/src/app/events/[details]/SourceTx.tsx
+++ apps/rosen/src/app/events/[details]/SourceTx.tsx
@@ -44,9 +44,10 @@ export const SourceTx = ({ id }: SourceTxProps) => {
       {!isLoading && data && (
         <Card backgroundColor="primary.light">
           <CardBody style={{ position: 'relative' }}>
-            <div style={{ position: 'absolute', right: 0, top: 10 }}>
-              <CopyButton value={data} />
-            </div>
+            <CopyButton
+              value={data}
+              style={{ position: 'absolute', right: 0, top: 10 }}
+            />
             <pre style={{ paddingTop: '16px', overflow: 'hidden' }}>{data}</pre>
           </CardBody>
         </Card>
