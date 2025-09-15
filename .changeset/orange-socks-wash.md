6906273199e3af600e651dbcdd0fc7f6a5424c16
Mohsen Javidpanah <moshen212@gmail.com>
2025-09-15 17:47:16 +0330

    Use @rosen-clients/rate-limited-axios instead of @rosen-bridge/rate-limited-axios

---
+++ .changeset/orange-socks-wash.md
@@ -0,0 +1,8 @@
+---
+'@rosen-ui/asset-calculator': minor
+'@rosen-bridge/rosen-service': minor
+'@rosen-network/cardano': minor
+'@rosen-network/ergo': minor
+---
+
+Use @rosen-clients/rate-limited-axios instead of @rosen-bridge/rate-limited-axios
--- apps/rosen-service/src/bootstrap/axios.ts
+++ apps/rosen-service/src/bootstrap/axios.ts
@@ -1,12 +1,12 @@
 import { CallbackLoggerFactory } from '@rosen-bridge/callback-logger';
-import { RateLimitedAxiosConfig } from '@rosen-bridge/rate-limited-axios';
+import { RateLimitedAxiosConfig } from '@rosen-clients/rate-limited-axios';
 
 import config from '../configs';
 
 const logger = CallbackLoggerFactory.getInstance().getLogger(import.meta.url);
 
 config.doge.rpcConnections.forEach((rpcConfig) => {
-  RateLimitedAxiosConfig.addRule(`^${rpcConfig.url}$`, 3, 1);
+  RateLimitedAxiosConfig.addRule(`^${rpcConfig.url}$`, 3, 1, 300);
 });
 
 RateLimitedAxiosConfig.setLogger(logger);
