58e1356de782b0f2c0930cbcd1085c16ed5f2bb5
Masood Abdolian <m.abdolian@gmail.com>
2025-09-29 06:58:18 +0000

    fix: Add Knip and update lint-staged configuration to integrate it for dependency checks

---
+++ .changeset/brave-corners-relate.md
@@ -0,0 +1,5 @@
+---
+'@rosen-bridge/ui-kit': patch
+---
+
+Add `Knip` and update lint-staged configuration to integrate it for dependency checks
--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -19,39 +19,13 @@ const perPackage = (resolver) => (files) => {
   );
 };
 
-const getDepcheckCommand = (directory) => {
-  const packages = [
-    '@eslint/js',
-    '@mui/material',
-    '@rosen-bridge/changeset-formatter',
-    '@trivago/prettier-plugin-sort-imports',
-    '@types/node',
-    '@types/react',
-    '@types/react-dom',
-    '@vitest/runner',
-    '@vitest/coverage-istanbul',
-    'eslint-config-prettier',
-    'lint-staged',
-    'pg',
-    'prettier',
-    'tsx',
-    'vitest',
-  ];
-
-  const paths = ['vite.config.ts.timestamp-*'];
-
-  return `npx depcheck --ignores="${packages.join(', ')}" --ignore-patterns="${paths.join(', ')}" ${path.relative(process.cwd(), directory)}`;
-};
-
 const configs = {
-  '*': 'prettier --ignore-unknown --write',
+  '*': ['prettier --ignore-unknown --write', () => 'knip --dependencies'],
   '**/{networks,packages,wallets,apps/guard,apps/rosen,apps/rosen-service2,apps/watcher}/**/*.{js,jsx,ts,tsx}':
     'eslint --fix',
   '**/*.{ts,tsx}': perPackage((directory) => {
     return `npm run type-check --workspace ${path.relative(process.cwd(), directory)}`;
   }),
-  '**/*.{js,jsx,ts,tsx,mjs}': perPackage(getDepcheckCommand),
-  '**/package.json': perPackage(getDepcheckCommand),
   '*.{js,jsx,ts,tsx}': 'npm run test:related',
 };
 
