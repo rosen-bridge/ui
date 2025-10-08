7886c2132439b66c9546b055713225a521da9caf
hazhirmohammadi <hazhir6690@gmail.com>
2025-10-08 12:23:20 +0330

    feat: Refine and standardize Divider component with extended props in ui-kit package

--- apps/rosen/src/app/assets/TableRow.tsx
+++ apps/rosen/src/app/assets/TableRow.tsx
@@ -313,7 +313,7 @@ export const TabletRow: FC<RowProps> = (props) => {
       >
         <EnhancedTableCell colSpan={10} padding="none">
           <Collapse in={open} unmountOnExit>
-            <Divider variant="middle" sx={{ borderBottomStyle: 'dashed' }} />
+            <Divider variant="middle" borderStyle="dashed" />
             {data && (
               <Box sx={{ m: 2 }}>
                 {data.bridged && (
