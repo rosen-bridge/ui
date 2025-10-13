633dcc44c261a9aa498190de290744f6fc33f5a2
FreaksOnE <freaksone66@gmail.com>
2025-10-13 15:41:54 +0330

    add test scenarios for 1 and 2 step selections

--- networks/bitcoin-runes/unitTests/generateUnsignedTx.spec.ts
+++ networks/bitcoin-runes/unitTests/generateUnsignedTx.spec.ts
@@ -1,37 +1,27 @@
-import { RosenAmount, TokenMap } from '@rosen-bridge/tokens';
+import { TokenMap } from '@rosen-bridge/tokens';
+import axios from 'axios';
+import { Mock } from 'vitest';
 
 import { generateUnsignedTx } from '../src';
 import { getAdditionalBoxes } from '../src/utils';
-import { fromAddress, internalPubkey, lockAddress, lockData } from './testData';
+import * as testData from './testData';
 
-vi.mock('axios', async () => {
-  const testData = await import('./testData');
+vi.mock('axios', () => {
   return {
     default: {
-      get: vi
-        .fn()
-        .mockResolvedValueOnce({ status: 200, data: testData.runesBoxes[0] })
-        .mockResolvedValueOnce({
-          status: 200,
-          data: testData.availableBtcBoxes[0],
-        })
-        .mockResolvedValueOnce({ status: 200, data: testData.allBtcBoxes[0] })
-        .mockResolvedValueOnce({ status: 200, data: testData.allBtcBoxes[1] })
-        .mockResolvedValueOnce({ status: 200, data: testData.allBtcBoxes[2] }),
+      get: vi.fn(),
     },
   };
 });
 
-vi.mock('../src/constants', () => {
+vi.mock('../src/constants', async (importOriginal) => {
+  const ref = await importOriginal<typeof import('../src/constants')>();
+
   return {
-    SEGWIT_INPUT_WEIGHT_UNIT: 272,
-    SEGWIT_OUTPUT_WEIGHT_UNIT: 124,
-    CONFIRMATION_TARGET: 6,
-    TAPROOT_INPUT_WEIGHT_UNIT: 230,
-    TAPROOT_OUTPUT_WEIGHT_UNIT: 172,
-    MINIMUM_BTC_FOR_NATIVE_SEGWIT_OUTPUT: 294n,
-    MINIMUM_BTC_FOR_TAPROOT_OUTPUT: 330n,
-    GET_BOX_API_LIMIT: 2,
+    ...ref,
+    default: {
+      GET_BOX_API_LIMIT: 2,
+    },
   };
 });
 
@@ -47,45 +37,140 @@ vi.mock('../src/utils', async (importOriginal) => {
 });
 
 describe('generateUnsignedTx', () => {
+  beforeEach(() => {
+    (getAdditionalBoxes as Mock).mockClear();
+    (axios.get as Mock).mockClear();
+  });
+
   /**
-   * @target generateUnsignedTx should perform 3 selection steps to get the required amounts
+   * @target generateUnsignedTx should perform 1 selection step to get the required amounts
    * @dependencies
    * @scenario
-   * - stub requestUnisat to return mock sequence of responses
+   * - stub axios.get to return mock sequence of responses
+   * - stub constants to return a mock value for GET_BOX_API_LIMIT
+   * - stub tokenMap.unwrapAmount to return a mock value
    * - spy on getAdditionalBoxes
    * - call generateUnsignedTx
    * - check the returned psbt
    * @expected
-   * - generateUnsignedTx should resolve to a psbt object
-   * - getAdditionalBoxes mock should have been called 2 times
+   * - returning inputSize should have been 1
+   * - getAdditionalBoxes mock should not have been called
    */
-  it('should perform 3 selection steps to get the required amounts', async () => {
+  it('should perform 1 selection step to get the required amounts', async () => {
     // arrange
+    (axios.get as Mock).mockResolvedValueOnce({
+      status: 200,
+      data: testData.runesBoxes2[0],
+    });
+
     const getTokenMap = async () =>
       ({
-        unwrapAmount: vi.fn().mockImplementation(
-          // eslint-disable-next-line @typescript-eslint/no-unused-vars
-          (tokenId: string, amount: bigint, toChain: string): RosenAmount => {
-            return { amount: 10n, decimals: 2 };
-          },
-        ),
+        unwrapAmount: vi
+          .fn()
+          .mockImplementation(() => ({ amount: 10n, decimals: 2 })),
       }) as unknown as TokenMap;
 
     // act
     const result = await generateUnsignedTx(getTokenMap)(
-      lockAddress,
-      fromAddress,
+      testData.lockAddress,
+      testData.fromAddress,
       100n,
-      lockData,
-      {
-        tokenId: '880352:855',
-        name: 'TESTINGCATAETCH',
-        decimals: 2,
-        type: 'type',
-        residency: 'wrapped',
-        extra: {},
-      },
-      internalPubkey,
+      testData.lockData,
+      testData.transferToken,
+      testData.internalPubkey,
+    );
+
+    // assert
+    expect(result.inputSize).toBe(1);
+    expect(getAdditionalBoxes).not.toHaveBeenCalled();
+  });
+
+  /**
+   * @target generateUnsignedTx should perform 2 selection step to get the required amounts
+   * @dependencies
+   * @scenario
+   * - stub axios.get to return mock sequence of responses
+   * - stub constants to return a mock value for GET_BOX_API_LIMIT
+   * - stub tokenMap.unwrapAmount to return a mock value
+   * - spy on getAdditionalBoxes
+   * - call generateUnsignedTx
+   * - check the returned psbt
+   * @expected
+   * - returning inputSize should have been 2
+   * - getAdditionalBoxes mock should have been called once
+   */
+  it('should perform 2 selection step to get the required amounts', async () => {
+    // arrange
+    (axios.get as Mock)
+      .mockResolvedValueOnce({ status: 200, data: testData.runesBoxes[0] })
+      .mockResolvedValueOnce({
+        status: 200,
+        data: testData.availableBtcBoxes2[0],
+      });
+
+    const getTokenMap = async () =>
+      ({
+        unwrapAmount: vi
+          .fn()
+          .mockImplementation(() => ({ amount: 10n, decimals: 2 })),
+      }) as unknown as TokenMap;
+
+    // act
+    const result = await generateUnsignedTx(getTokenMap)(
+      testData.lockAddress,
+      testData.fromAddress,
+      100n,
+      testData.lockData,
+      testData.transferToken,
+      testData.internalPubkey,
+    );
+
+    // assert
+    expect(result.inputSize).toBe(2);
+    expect(getAdditionalBoxes).toHaveBeenCalledTimes(1);
+  });
+
+  /**
+   * @target generateUnsignedTx should perform 3 selection steps to get the required amounts
+   * @dependencies
+   * @scenario
+   * - stub axios.get to return mock sequence of responses
+   * - stub constants to return a mock value for GET_BOX_API_LIMIT
+   * - stub tokenMap.unwrapAmount to return a mock value
+   * - spy on getAdditionalBoxes
+   * - call generateUnsignedTx
+   * - check the returned psbt
+   * @expected
+   * - returning inputSize should have been 2
+   * - getAdditionalBoxes mock should have been called 2 times
+   */
+  it('should perform 3 selection steps to get the required amounts', async () => {
+    // arrange
+    (axios.get as Mock)
+      .mockResolvedValueOnce({ status: 200, data: testData.runesBoxes[0] })
+      .mockResolvedValueOnce({
+        status: 200,
+        data: testData.availableBtcBoxes[0],
+      })
+      .mockResolvedValueOnce({ status: 200, data: testData.allBtcBoxes[0] })
+      .mockResolvedValueOnce({ status: 200, data: testData.allBtcBoxes[1] })
+      .mockResolvedValueOnce({ status: 200, data: testData.allBtcBoxes[2] });
+
+    const getTokenMap = async () =>
+      ({
+        unwrapAmount: vi
+          .fn()
+          .mockImplementation(() => ({ amount: 10n, decimals: 2 })),
+      }) as unknown as TokenMap;
+
+    // act
+    const result = await generateUnsignedTx(getTokenMap)(
+      testData.lockAddress,
+      testData.fromAddress,
+      100n,
+      testData.lockData,
+      testData.transferToken,
+      testData.internalPubkey,
     );
 
     // assert
--- networks/bitcoin-runes/unitTests/testData.ts
+++ networks/bitcoin-runes/unitTests/testData.ts
@@ -18,6 +18,15 @@ export const lockData = crypto.randomBytes(80).toString('hex');
 export const internalPubkey =
   '187791b6f712a8ea41c8ecdd0ee77fab3e85263b37e1ec18a3651926b3a6cf27';
 
+export const transferToken = {
+  tokenId: '880352:855',
+  name: 'TESTINGCATAETCH',
+  decimals: 2,
+  type: 'type',
+  residency: 'wrapped',
+  extra: {},
+};
+
 export const runesBoxes: UnisatResponse<UnisatAddressRunesUtxos>[] = [
   {
     code: 0,
@@ -61,6 +70,40 @@ export const runesBoxes: UnisatResponse<UnisatAddressRunesUtxos>[] = [
   },
 ];
 
+export const runesBoxes2: UnisatResponse<UnisatAddressRunesUtxos>[] = [
+  {
+    code: 0,
+    data: {
+      height: 918734,
+      start: 0,
+      total: 1,
+      utxo: [
+        {
+          height: 915580,
+          confirmations: 3155,
+          address:
+            'bc1pjxzw9tm6qatyapu3c409dg8k23p4hjlk4ehwwlsum3emjqsaetrqppyu2z',
+          satoshi: 5000,
+          scriptPk:
+            '512033fadad0034bb0a7912d2bbf2c9163627ba5ec7833341418595418c33deb0fb7',
+          txid: 'ba9bd0e9c84b2743313d3b7f34777e9dab0688fc85fa9cce8cbb340f8d06ffcb',
+          vout: 0,
+          runes: [
+            {
+              rune: 'TESTINGCATAETCH',
+              runeid: '880352:855',
+              spacedRune: 'TESTING•CATA•ETCH',
+              amount: '4492999000',
+              symbol: 'H',
+              divisibility: 2,
+            },
+          ],
+        },
+      ],
+    },
+  },
+];
+
 export const availableBtcBoxes: UnisatResponse<UnisatAddressAvailableBtcUtxos>[] =
   [
     {
@@ -73,6 +116,39 @@ export const availableBtcBoxes: UnisatResponse<UnisatAddressAvailableBtcUtxos>[]
       },
     },
   ];
+
+export const availableBtcBoxes2: UnisatResponse<UnisatAddressAvailableBtcUtxos>[] =
+  [
+    {
+      code: 0,
+      msg: 'ok',
+      data: {
+        cursor: 0,
+        total: 1,
+        utxo: [
+          {
+            confirmations: 10,
+            txid: '22b93e565aaa62ab0fa1471fcaabaa7e665a4d3d3623186bd36a241cbe65f850',
+            vout: 0,
+            satoshi: 55133,
+            scriptType: '5120',
+            scriptPk:
+              '512033fadad0034bb0a7912d2bbf2c9163627ba5ec7833341418595418c33deb0fb7',
+            codeType: 9,
+            address:
+              'bc1pjxzw9tm6qatyapu3c409dg8k23p4hjlk4ehwwlsum3emjqsaetrqppyu2z',
+            height: 915891,
+            idx: 669,
+            isOpInRBF: false,
+            isSpent: false,
+            inscriptionsCount: 1,
+            inscriptions: [],
+          },
+        ],
+      },
+    },
+  ];
+
 export const allBtcBoxes: UnisatResponse<UnisatAddressBtcUtxos>[] = [
   {
     code: 0,
