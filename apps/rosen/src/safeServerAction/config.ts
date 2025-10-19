fdaf9929762de08b3639e104ca966dcedb93e320
Masood Abdolian <m.abdolian@gmail.com>
2025-10-19 12:30:55 +0000

    feat: Suitable error to be thrown for uncovered assets in networks package and user to be informed in Rosen app

--- apps/rosen/src/safeServerAction/config.ts
+++ apps/rosen/src/safeServerAction/config.ts
@@ -1,23 +1,25 @@
+import { InsufficientAssetsError } from '@rosen-network/base/dist/handleUncoveredAssets';
 import { serializeError } from 'serialize-error';
 
 import { createSafeAction } from './safeServerAction';
 
 export const { wrap, unwrap, unwrapFromObject } = createSafeAction({
+  errors: {
+    InsufficientAssetsError,
+  },
   async onError(error, traceKey, args) {
-    if (typeof window === 'undefined') {
-      import('@/actions')
-        .then(({ logger }) => {
-          logger(traceKey, args, serializeError(error))
-            .then(() => {
-              console.log('Sent log to Discord successfully');
-            })
-            .catch((error) => {
-              console.log('Failed to send log to Discord', error);
-            });
-        })
-        .catch((error) => {
-          console.log('Failed to load logger utils', error);
-        });
+    try {
+      if (typeof window !== 'undefined') return;
+
+      if (error instanceof InsufficientAssetsError) return;
+
+      const { logger } = await import('@/actions');
+
+      await logger(traceKey, args, serializeError(error));
+
+      console.log('Sent log to Discord successfully');
+    } catch (error) {
+      console.log('Failed to send log to Discord', error);
     }
   },
 });
