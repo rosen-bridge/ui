35b1ec6e05ddebcf802afc3dcbbf0f7cbbc70f1e
hazhirmohammadi <hazhir6690@gmail.com>
2025-10-25 16:54:19 +0330

    feat: replace mui divider with custom divider in ui-kit package

--- packages/ui-kit/src/components/common/smartSearch/History.tsx
+++ packages/ui-kit/src/components/common/smartSearch/History.tsx
@@ -8,7 +8,7 @@ import {
   useState,
 } from 'react';
 
-import { ClickAwayListener, Divider } from '@mui/material';
+import { ClickAwayListener } from '@mui/material';
 import { Favorite, History as HistoryIcon, Times } from '@rosen-bridge/icons';
 
 import { useBreakpoint } from '../../../hooks';
@@ -20,6 +20,7 @@ import {
   ListSubheader,
 } from '../../base';
 import { IconButton } from '../../base';
+import { Divider } from '../Divider';
 import { SvgIcon } from '../SvgIcon';
 import { Truncate } from '../Truncate';
 import { Popup } from './Popup';
