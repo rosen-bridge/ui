1445238e1608abc4985ea06ff81060039578ab4c
Masood Abdolian <m.abdolian@gmail.com>
2025-10-26 06:22:44 +0000

    feat: Suitable error to be thrown for uncovered assets in networks package and user to be informed in Rosen app

---
+++ networks/base/src/handleUncoveredAssets.ts
@@ -0,0 +1,69 @@
+import { AssetBalance } from '@rosen-bridge/selection-types';
+import { RosenChainToken, TokenMap } from '@rosen-bridge/tokens';
+import { NETWORKS } from '@rosen-ui/constants';
+import { Network } from '@rosen-ui/types';
+import { getDecimalString } from '@rosen-ui/utils';
+
+export class InsufficientAssetsError extends Error {
+  public name = 'InsufficientAssetsError';
+  constructor(
+    public values: string[] = [],
+    public cause?: unknown,
+  ) {
+    super(
+      `The transaction cannot be completed because at least ${values.join(' and ')} ${values.length === 1 ? 'is' : 'are'} still required to cover fees or minimum box requirements.`,
+      {
+        cause,
+      },
+    );
+  }
+}
+
+/**
+ * Handles cases where an asset balance does not cover required values.
+ *
+ * @param tokenMap
+ * @param nativeTokenKey
+ * @param assetBalance
+ */
+export const handleUncoveredAssets = (
+  tokenMap: TokenMap,
+  nativeTokenKey: Network,
+  assetBalance?: AssetBalance,
+) => {
+  const values: string[] = [];
+
+  const nativeToken = tokenMap
+    .search(nativeTokenKey, { tokenId: NETWORKS[nativeTokenKey].nativeToken })
+    .at(0)![nativeTokenKey];
+
+  if (assetBalance && assetBalance?.nativeToken !== 0n) {
+    const decimals = nativeToken.decimals;
+
+    const amount = getDecimalString(assetBalance.nativeToken, decimals);
+
+    const value = `${amount} ${nativeToken.name}`;
+
+    values.push(value);
+  }
+
+  for (const tokenInfo of assetBalance?.tokens || []) {
+    const token: RosenChainToken = tokenMap
+      .search(nativeTokenKey, { tokenId: tokenInfo.id })
+      .at(0)![nativeTokenKey];
+
+    const decimals = token.decimals;
+
+    const amount = getDecimalString(tokenInfo.value, decimals);
+
+    const value = `${amount} ${token.name}`;
+
+    values.push(value);
+  }
+
+  if (!values.length) {
+    throw new Error('Unexpected Error');
+  }
+
+  throw new InsufficientAssetsError(values);
+};
