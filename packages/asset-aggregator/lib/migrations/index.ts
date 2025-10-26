6467c230d147b2a6f02c33981a0fd6948c03ff80
Mohsen Javidpanah <moshen212@gmail.com>
2025-10-26 15:13:37 +0330

    Restore sqlite migration files

--- packages/asset-aggregator/lib/migrations/index.ts
+++ packages/asset-aggregator/lib/migrations/index.ts
@@ -1,5 +1,7 @@
 import { Migration1759776111031 } from './postgres';
+import { Migration1761477848161 } from './sqlite';
 
 export const migrations = {
   postgres: [Migration1759776111031],
+  sqlite: [Migration1761477848161],
 };
---
+++ packages/asset-aggregator/lib/migrations/sqlite/index.ts
@@ -0,0 +1,1 @@
+export { Migration1761477848161 } from './1761477848161-migration';
