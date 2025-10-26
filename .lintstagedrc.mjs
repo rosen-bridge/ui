6e8a0f11b77d158c63cd06c93d1951043ef120f1
Mohsen Javidpanah <moshen212@gmail.com>
2025-10-26 16:03:31 +0330

    Updated lintstage file

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -4,7 +4,7 @@ import path from 'path';
 const perPackage = (resolver) => (files) => {
   return Array.from(
     files.reduce((packages, file) => {
-      let directory = path.dirname(file);
+      let directory = path.dirname(path.resolve(file));
       while (directory && directory !== process.cwd()) {
         if (fs.existsSync(path.join(directory, 'package.json'))) {
           packages.add(resolver(directory, file));
@@ -19,8 +19,25 @@ const perPackage = (resolver) => (files) => {
   );
 };
 
+const getKnipCommand = (dir) => {
+  const posixRelative = path.posix.relative(process.cwd(), dir);
+  return `npx knip --dependencies --workspace ${posixRelative}`;
+};
+
+const runKnipConditional = (files) => {
+  const rootChanged = files.some((f) => {
+    const relative = path.relative(process.cwd(), path.resolve(f));
+    return !relative.includes(path.sep);
+  });
+  if (rootChanged) {
+    return ['npx knip --dependencies'];
+  } else {
+    return perPackage(getKnipCommand)(files);
+  }
+};
+
 const configs = {
-  '*': ['prettier --ignore-unknown --write', () => 'knip --dependencies'],
+  '*': ['prettier --ignore-unknown --write', runKnipConditional],
   '**/{networks,packages,wallets,apps/guard,apps/rosen,apps/rosen-service2,apps/watcher}/**/*.{js,jsx,ts,tsx}':
     'eslint --fix',
   '**/*.{ts,tsx}': perPackage((directory) => {
