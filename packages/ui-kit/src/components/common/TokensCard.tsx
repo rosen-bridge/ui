810e1459d540821f7c1b995c83e9e036ddac8fee
Masood Abdolian <m.abdolian@gmail.com>
2025-11-12 14:13:44 +0000

    feat: Update tokens section to match latest design in guard app

--- packages/ui-kit/src/components/common/TokensCard.tsx
+++ packages/ui-kit/src/components/common/TokensCard.tsx
@@ -1,9 +1,10 @@
-import { TokenInfoWithColdAmount } from '@rosen-ui/types';
+import { Network, TokenInfoWithColdAmount } from '@rosen-ui/types';
 
-import { FullCard } from '.';
-import { TokensList } from './tokensCard/TokensList';
+import { FullCard } from './FullCard';
+import { TokensList } from './TokensList';
 
 export interface TokensCardProps {
+  chain?: Network;
   title: string;
   tokens: TokenInfoWithColdAmount[];
   isLoading: boolean;
@@ -12,16 +13,22 @@ export interface TokensCardProps {
  * a wrapper for `TokensList` which also renders a title and a "See All" action
  * button
  *
+ * @param chain
  * @param title
  * @param tokens
  * @param isLoading
  */
-export const TokensCard = ({ title, tokens, isLoading }: TokensCardProps) => (
+export const TokensCard = ({
+  chain,
+  title,
+  tokens,
+  isLoading,
+}: TokensCardProps) => (
   /**
    * TODO: Implement the See All Section and click handler as part of #9
    * local:ergo/rosen-bridge/ui#9
    */
   <FullCard title={title}>
-    <TokensList tokens={tokens ?? []} isLoading={isLoading} />
+    <TokensList chain={chain} tokens={tokens ?? []} isLoading={isLoading} />
   </FullCard>
 );
