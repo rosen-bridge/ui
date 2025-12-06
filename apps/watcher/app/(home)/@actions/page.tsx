12920057ed043613ff0bc5c454b83b695cf78c32
Masood Abdolian <m.abdolian@gmail.com>
2025-12-06 09:20:19 +0000

    refactor: Update dashboard components for new design in watcher app

--- apps/watcher/app/(home)/@actions/page.tsx
+++ apps/watcher/app/(home)/@actions/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+
 import {
   LockAlt,
   MoneyWithdrawal,
@@ -7,66 +9,94 @@ import {
   SquareShape,
   Unlock,
 } from '@rosen-bridge/icons';
-import { FullCard, SvgIcon } from '@rosen-bridge/ui-kit';
+import { Button, Divider, Stack, styled, SvgIcon } from '@rosen-bridge/ui-kit';
 
-import { HomeActionButton } from '@/_components/HomeActionButton';
+const HomeActionButtonBase = styled(Button)(({ theme }) => ({
+  'display': 'flex',
+  'flexDirection': 'column',
+  'gap': theme.spacing(0.5),
+  'borderRadius': 0,
+  'color': theme.palette.primary.main,
+  'backgroundColor': theme.palette.primary.light,
+  'flexBasis': '20%',
+  '&:hover': {
+    color: theme.palette.primary.contrastText,
+    backgroundColor: theme.palette.primary.dark,
+  },
+  [theme.breakpoints.down('tablet')]: {
+    fontSize: 0,
+    gap: 0,
+  },
+}));
 
 const Actions = () => {
+  const router = useRouter();
+
   return (
-    <FullCard
-      title="Actions"
-      cardActions={
-        <>
-          <HomeActionButton
-            action="withdraw"
-            label="Withdraw"
-            icon={
-              <SvgIcon>
-                <MoneyWithdrawal />
-              </SvgIcon>
-            }
-          />
-          <HomeActionButton
-            action="pause"
-            label="Pause"
-            disabled={true}
-            icon={
-              <SvgIcon>
-                <Pause />
-              </SvgIcon>
-            }
-          />
-          <HomeActionButton
-            action="stop"
-            label="Stop"
-            disabled={true}
-            icon={
-              <SvgIcon>
-                <SquareShape />
-              </SvgIcon>
-            }
-          />
-          <HomeActionButton
-            action="lock"
-            label="Lock"
-            icon={
-              <SvgIcon>
-                <LockAlt />
-              </SvgIcon>
-            }
-          />
-          <HomeActionButton
-            action="unlock"
-            label="Unlock"
-            icon={
-              <SvgIcon>
-                <Unlock />
-              </SvgIcon>
-            }
-          />
-        </>
-      }
-    />
+    <Stack
+      direction="row"
+      style={{
+        borderRadius: '16px',
+        overflow: 'hidden',
+      }}
+    >
+      <HomeActionButtonBase
+        startIcon={
+          <SvgIcon>
+            <MoneyWithdrawal />
+          </SvgIcon>
+        }
+        onClick={() => router.push('/actions/withdraw')}
+      >
+        Withdraw
+      </HomeActionButtonBase>
+      <Divider orientation="vertical" />
+      <HomeActionButtonBase
+        disabled={true}
+        startIcon={
+          <SvgIcon>
+            <Pause />
+          </SvgIcon>
+        }
+        onClick={() => router.push('/actions/pause')}
+      >
+        Pause
+      </HomeActionButtonBase>
+      <Divider orientation="vertical" />
+      <HomeActionButtonBase
+        disabled={true}
+        startIcon={
+          <SvgIcon>
+            <SquareShape />
+          </SvgIcon>
+        }
+        onClick={() => router.push('/actions/stop')}
+      >
+        Stop
+      </HomeActionButtonBase>
+      <Divider orientation="vertical" />
+      <HomeActionButtonBase
+        startIcon={
+          <SvgIcon>
+            <LockAlt />
+          </SvgIcon>
+        }
+        onClick={() => router.push('/actions/lock')}
+      >
+        Lock
+      </HomeActionButtonBase>
+      <Divider orientation="vertical" />
+      <HomeActionButtonBase
+        startIcon={
+          <SvgIcon>
+            <Unlock />
+          </SvgIcon>
+        }
+        onClick={() => router.push('/actions/unlock')}
+      >
+        Unlock
+      </HomeActionButtonBase>
+    </Stack>
   );
 };
 
