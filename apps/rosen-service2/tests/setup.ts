8b6b41b2ac0ae92873015b4c874e6b07fd69f1a6
Mohsen Javidpanah <moshen212@gmail.com>
2025-12-20 13:48:21 +0330

    Revert tests setup file to old correct state

--- apps/rosen-service2/tests/setup.ts
+++ apps/rosen-service2/tests/setup.ts
@@ -1,7 +1,1 @@
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-
 import '../src/bootstrap';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-process.env.NODE_CONFIG_DIR = path.resolve(__dirname, '../config');
