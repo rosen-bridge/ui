9878cbc6fec921f1af97577200e6d96ff8101b1d
rasoul salehi <8raha6094@gmail.com>
2025-12-25 17:54:12 +0330

    feat: add rosen-statistics-entity with entities and migrations

---
+++ packages/rosen-statistics-entity/README.md
@@ -0,0 +1,24 @@
+# rosen-statistics-entity
+
+## Table of contents
+
+- [Introduction](#introduction)
+- [Installation](#installation)
+
+## Introduction
+
+Rosen statistics entity and migrations
+
+## Installation
+
+npm:
+
+```sh
+npm i rosen-statistics-entity
+```
+
+yarn:
+
+```sh
+yarn add rosen-statistics-entity
+```
---
+++ packages/rosen-statistics-entity/lib/entities/userEventEntity.ts
@@ -0,0 +1,22 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  Unique,
+} from '@rosen-bridge/extended-typeorm';
+
+@Unique(['fromAddress', 'toAddress'])
+@Entity('user_event_entity')
+export class UserEventEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar' })
+  fromAddress: string;
+
+  @Column({ type: 'varchar' })
+  toAddress: string;
+
+  @Column({ type: 'int' })
+  count: number;
+}
