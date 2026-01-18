0d2896bb95fd7c89c3fd4f981dcb353445e234ef
Masood Abdolian <m.abdolian@gmail.com>
2026-01-18 09:12:55 +0000

    init

--- apps/watcher/src/app/SideBar.tsx
+++ apps/watcher/src/app/SideBar.tsx
@@ -29,12 +29,7 @@ export const SideBar = () => {
     <AppBar
       logo={
         <Link href="/">
-          <AppLogo
-            darkLogoPath="/logo-dark-desktop.png"
-            lightLogoPath="/logo-light-desktop.png"
-            darkLogoMobilePath="/logo-dark-mobile.png"
-            lightLogoMobilePath="/logo-light-mobile.png"
-          />
+          <AppLogo />
         </Link>
       }
       versions={<VersionConfig />}
