a0d4a968f3b5e4a75d8e3ffd6d0bb3e439d8a7af
Masood Abdolian <m.abdolian@gmail.com>
2026-02-24 01:32:31 +0330

    update

--- apps/rosen/global.d.ts
+++ apps/rosen/global.d.ts
@@ -1,4 +1,12 @@
+import type { DefaultColor } from '@rosen-bridge/ui-kit';
+
 declare module '@rosen-bridge/ui-kit' {
+  interface ColorOverrides extends Record<DefaultColor, true> {
+    UNLISTED: false;
+  }
+
+  interface GapOverrides {}
+
   interface IconOverrides {}
 }
 export {};
--- packages/ui-kit/src/@types/color.ts
+++ packages/ui-kit/src/@types/color.ts
@@ -6,3 +6,26 @@ export type Color = string & {};
 export interface ColorOverrides {}
 
 export type ColorOverridden = OverridableValue<Color, ColorOverrides>;
+
+type ColorName =
+  | 'primary'
+  | 'secondary'
+  | 'tertiary'
+  | 'error'
+  | 'warning'
+  | 'info'
+  | 'success'
+  | 'neutral';
+
+type ColorShade = 'light' | 'dark' | 'contrastText';
+
+export type DefaultColor =
+  | 'inherit'
+  | `${ColorName}-${ColorShade}`
+  | ColorName
+  | 'background'
+  | 'background-default'
+  | 'background-paper'
+  | 'text-primary'
+  | 'text-secondary'
+  | 'text-disabled';
