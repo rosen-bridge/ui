3199ca16b1544260f74749531e9b6be114dc990f
Masood Abdolian <m.abdolian@gmail.com>
2026-02-24 00:06:17 +0330

    update

--- apps/rosen/src/app/App.tsx
+++ apps/rosen/src/app/App.tsx
@@ -10,13 +10,22 @@ import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { PropsWithChildren } from 'react';
 
 import { NoSsr } from '@mui/material';
-import { App as AppBase, FrameworkProvider } from '@rosen-bridge/ui-kit';
+import * as AllIcons from '@rosen-bridge/icons';
+import {
+  App as AppBase,
+  ConfigProvider,
+  FrameworkProvider,
+} from '@rosen-bridge/ui-kit';
 
 import { theme } from '@/theme/theme';
 
 import { TokenMapProvider } from '../hooks';
 import { SideBar } from './SideBar';
 import { Toolbar } from './Toolbar';
+
+const icons = Object.fromEntries(
+  Object.entries(AllIcons).filter(([key]) => key !== 'TOKENS'),
+);
 
 export const App = ({ children }: PropsWithChildren) => {
   const pathname = usePathname();
@@ -27,21 +36,29 @@ export const App = ({ children }: PropsWithChildren) => {
 
   return (
     <NoSsr>
-      <FrameworkProvider
-        components={{
-          Anchor: (props) => <NextLink {...props} />,
-          Image: (props) => <NextImage {...props} />,
-        }}
-        router={{
-          pathname,
-          search: searchParams.toString(),
-          push: (href: string) => router.push(href, { scroll: false }),
+      <ConfigProvider
+        configs={{
+          components: {
+            Icon: { defaultProps: { icons } },
+          },
         }}
       >
-        <AppBase sideBar={<SideBar />} theme={theme} toolbar={<Toolbar />}>
-          <TokenMapProvider>{children}</TokenMapProvider>
-        </AppBase>
-      </FrameworkProvider>
+        <FrameworkProvider
+          components={{
+            Anchor: (props) => <NextLink {...props} />,
+            Image: (props) => <NextImage {...props} />,
+          }}
+          router={{
+            pathname,
+            search: searchParams.toString(),
+            push: (href: string) => router.push(href, { scroll: false }),
+          }}
+        >
+          <AppBase sideBar={<SideBar />} theme={theme} toolbar={<Toolbar />}>
+            <TokenMapProvider>{children}</TokenMapProvider>
+          </AppBase>
+        </FrameworkProvider>
+      </ConfigProvider>
     </NoSsr>
   );
 };
