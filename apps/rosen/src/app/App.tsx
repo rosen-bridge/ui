6bb858ca4b97f388693a21ccf7255b228cb5ae82
Masood Abdolian <m.abdolian@gmail.com>
2026-03-11 22:12:44 +0330

    update

--- apps/rosen/src/app/App.tsx
+++ apps/rosen/src/app/App.tsx
@@ -5,34 +5,22 @@
  * local:ergo/rosen-bridge/ui#193
  */
 import { Route } from 'next';
-import NextImage from 'next/image';
-import NextLink from 'next/link';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { PropsWithChildren } from 'react';
 
 import { NoSsr } from '@mui/material';
-import * as AllIcons from '@rosen-bridge/icons';
 import {
   App as AppBase,
   ConfigProvider,
   FrameworkProvider,
 } from '@rosen-bridge/ui-kit';
-import { NETWORKS } from '@rosen-ui/constants';
 
 import { theme } from '@/theme/theme';
 
-import { TOKENS_MAPPER } from '../../configs';
 import { TokenMapProvider } from '../hooks';
+import { configs } from '../ui-kit.config';
 import { SideBar } from './SideBar';
 import { Toolbar } from './Toolbar';
-
-function kebabToPascal(value: string): string {
-  return value
-    .split('-')
-    .filter(Boolean)
-    .map((part) => part.charAt(0).toUpperCase() + part.slice(1))
-    .join('');
-}
 
 export const App = ({ children }: PropsWithChildren) => {
   const pathname = usePathname();
@@ -43,61 +31,7 @@ export const App = ({ children }: PropsWithChildren) => {
 
   return (
     <NoSsr>
-      <ConfigProvider
-        configs={{
-          components: {
-            Image: {
-              defaultProps: {
-                as: NextImage,
-              },
-            },
-            Token: {
-              defaultProps: {
-                tokens: Object.fromEntries(
-                  Object.entries(TOKENS_MAPPER).map(
-                    ([tokenId, { name, ergoSideTokenId }]) => [
-                      tokenId,
-                      {
-                        label: name,
-                        logo: AllIcons.TOKENS[
-                          ergoSideTokenId as keyof (typeof AllIcons)['TOKENS']
-                        ],
-                      },
-                    ],
-                  ),
-                ),
-              },
-            },
-            Icon: {
-              defaultProps: {
-                icons: Object.fromEntries(
-                  Object.entries(AllIcons).filter(([key]) => key !== 'TOKENS'),
-                ),
-              },
-            },
-            Link: {
-              defaultProps: {
-                as: NextLink,
-              },
-            },
-            Network: {
-              defaultProps: {
-                networks: Object.fromEntries(
-                  Object.entries(NETWORKS).map(([key, value]) => [
-                    key,
-                    {
-                      label: value.label,
-                      logo: AllIcons[
-                        kebabToPascal(key) as keyof typeof AllIcons
-                      ],
-                    },
-                  ]),
-                ),
-              },
-            },
-          },
-        }}
-      >
+      <ConfigProvider configs={configs}>
         <FrameworkProvider
           router={{
             pathname,
@@ -107,7 +41,6 @@ export const App = ({ children }: PropsWithChildren) => {
           }}
         >
           <AppBase sideBar={<SideBar />} theme={theme} toolbar={<Toolbar />}>
-            {/* <img src={AllIcons.TOKENS.erg}/> */}
             <TokenMapProvider>{children}</TokenMapProvider>
           </AppBase>
         </FrameworkProvider>
---
+++ apps/rosen/src/ui-kit.config.ts
@@ -0,0 +1,86 @@
+import { Route } from 'next';
+import NextImage from 'next/image';
+import NextLink from 'next/link';
+
+import * as AllIcons from '@rosen-bridge/icons';
+import type { ConfigProviderProps, DefaultColor } from '@rosen-bridge/ui-kit';
+import { NETWORKS } from '@rosen-ui/constants';
+import { Network } from '@rosen-ui/types';
+
+import { TOKENS_MAPPER } from '../configs';
+
+declare module '@rosen-bridge/ui-kit' {
+  interface ColorOverrides extends Record<DefaultColor, true> {
+    UNLISTED: false;
+  }
+
+  interface IconOverrides {
+    name: Exclude<keyof typeof AllIcons, 'TOKENS'>;
+  }
+
+  interface LinkOverrides {
+    href: Route;
+  }
+
+  interface NetworkOverrides {
+    value: Network;
+  }
+}
+
+export const configs: ConfigProviderProps['configs'] = {
+  components: {
+    Icon: {
+      defaultProps: {
+        icons: Object.fromEntries(
+          Object.entries(AllIcons).filter(([key]) => key !== 'TOKENS'),
+        ),
+      },
+    },
+    Image: {
+      defaultProps: {
+        as: NextImage,
+      },
+    },
+    Link: {
+      defaultProps: {
+        as: NextLink,
+      },
+    },
+    Network: {
+      defaultProps: {
+        networks: Object.fromEntries(
+          Object.entries(NETWORKS).map(([key, value]) => [
+            key,
+            {
+              label: value.label,
+              logo: AllIcons[
+                key
+                  .split('-')
+                  .filter(Boolean)
+                  .map((part) => part.charAt(0).toUpperCase() + part.slice(1))
+                  .join('') as keyof typeof AllIcons
+              ],
+            },
+          ]),
+        ),
+      },
+    },
+    Token: {
+      defaultProps: {
+        tokens: Object.fromEntries(
+          Object.entries(TOKENS_MAPPER).map(
+            ([tokenId, { name, ergoSideTokenId }]) => [
+              tokenId,
+              {
+                label: name,
+                logo: AllIcons.TOKENS[
+                  ergoSideTokenId as keyof (typeof AllIcons)['TOKENS']
+                ],
+              },
+            ],
+          ),
+        ),
+      },
+    },
+  },
+};
