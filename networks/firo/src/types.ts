f5201c4ac83900edb03188d5eb125a7718d41121
Reuben Yap <reuben@firo.org>
2026-04-29 20:21:23 +0800

    Use Firo RPC for wallet utilities

--- networks/firo/src/types.ts
+++ networks/firo/src/types.ts
@@ -1,41 +1,42 @@
-export interface Status {
-  confirmed: boolean;
-  block_height?: number;
-  block_hash?: string;
-  block_time?: number;
-}
-
 export interface FiroUtxo {
   txId: string;
   index: number;
   value: bigint;
 }
 
-// BlockCypher types that match our needs
-export interface BlockCypherAddress {
-  address: string;
-  total_received: number;
-  total_sent: number;
-  balance: number;
-  unconfirmed_balance: number;
-  final_balance: number;
-  n_tx: number;
-  unconfirmed_n_tx: number;
-  final_n_tx: number;
-  txrefs: BlockCypherTxRef[];
+export interface FiroRpcError {
+  code: number;
+  message: string;
 }
 
-export interface BlockCypherTxRef {
-  tx_hash: string;
-  block_height: number;
-  tx_input_n: number;
-  tx_output_n: number;
-  value: number;
-  ref_balance: number;
-  spent: boolean;
-  confirmations: number;
-  confirmed: string;
-  double_spend: boolean;
+export interface FiroRpcResponse<T> {
+  result: T;
+  error: FiroRpcError | null;
+  id: string;
+}
+
+export interface FiroRpcUtxo {
+  address: string;
+  txid: string;
+  outputIndex: number;
+  script: string;
+  satoshis: number;
+  height: number;
+}
+
+export interface FiroRpcBalance {
+  balance: number;
+  received: number;
+}
+
+export interface FiroRpcSmartFee {
+  feerate?: number;
+  errors?: string[];
+  blocks?: number;
+}
+
+export interface FiroRpcNetworkInfo {
+  relayfee?: number;
 }
 
 export interface UnsignedPsbtData {
