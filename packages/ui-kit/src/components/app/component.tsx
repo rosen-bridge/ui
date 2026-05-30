52a5129462952882830a3813098ab7505d0345c8
Masood Abdolian <m.abdolian@gmail.com>
2026-05-30 11:14:50 +0330

    update

--- packages/ui-kit/src/components/app/component.tsx
+++ packages/ui-kit/src/components/app/component.tsx
@@ -1,7 +1,9 @@
 import { ReactNode } from 'react';
 
+import { useMediaQuery } from '@mui/material';
+
 import { CssBaseline } from '@/components';
-import { useBreakpoint, useConfig } from '@/hooks';
+import { useBreakpoint, useConfig, useTheme } from '@/hooks';
 import { ThemeProvider, ThemeProviderProps, ToastProvider } from '@/Providers';
 import { ElementBaseProps, OverridableType } from '@/types';
 
@@ -21,9 +23,24 @@ export type AppBaseProps = ElementBaseProps<'div', AppOwnProps>;
 export type AppProps = OverridableType<AppBaseProps, AppOverrides, never>;
 
 const Content = ({ children, sidebar, ...rest }: AppProps) => {
+  const theme = useTheme();
+
   const isTabletDown = useBreakpoint('tablet-down');
+
+  const isMobile = useMediaQuery(theme.breakpoints.up('mobile'));
+  const isTablet = useMediaQuery(theme.breakpoints.up('tablet'));
+  const isLaptop = useMediaQuery(theme.breakpoints.up('laptop'));
+  const isDesktop = useMediaQuery(theme.breakpoints.up('desktop'));
+
   return (
-    <div data-orientation={isTabletDown ? 'vertical' : 'horizontal'} {...rest}>
+    <div
+      data-breakpoint-mobile={isMobile ? '' : null}
+      data-breakpoint-tablet={isTablet ? '' : null}
+      data-breakpoint-laptop={isLaptop ? '' : null}
+      data-breakpoint-desktop={isDesktop ? '' : null}
+      data-orientation={isTabletDown ? 'vertical' : 'horizontal'}
+      {...rest}
+    >
       {sidebar}
       <div className="RosenApp-main">
         <div className="RosenApp-paper">{children}</div>
