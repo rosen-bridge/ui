abbe6ecfae5c1e5648c91560bf1bd683e3fe7272
hazhirmohammadi <hazhir6690@gmail.com>
2026-06-14 22:39:29 +0330

    feat: add under development pages to Support and Dashboard in Rosen App and add illustration to UI Kit

--- apps/rosen/src/app/(main)/dashboard/page.tsx
+++ apps/rosen/src/app/(main)/dashboard/page.tsx
@@ -1,5 +1,30 @@
 'use client';
 
-const Dashboard = () => null;
+import { Fragment } from 'react';
+
+import {
+  CubeNetBackground,
+  Stack,
+  UnderDevelopment,
+} from '@rosen-bridge/ui-kit';
+
+const Dashboard = () => (
+  <Fragment>
+    <CubeNetBackground style={{ position: 'absolute', inset: 0, zIndex: 0 }} />
+    <Stack
+      align="center"
+      direction="column"
+      justify="center"
+      spacing={6}
+      style={{
+        zIndex: 10,
+        minHeight: 'calc(100vh - 224px)',
+        position: 'relative',
+      }}
+    >
+      <UnderDevelopment />
+    </Stack>
+  </Fragment>
+);
 
 export default Dashboard;
